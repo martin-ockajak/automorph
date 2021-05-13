974b7dc6c9be64df0b3f99bcca5439504333bb66
Martin Ockajak <martin.ockajak@drion.ai>
2021-05-14 01:02:51 +0200

    Method invocation prototype

--- src/main/scala/jsonrpc/server/ServerMacros.scala
+++ src/main/scala/jsonrpc/server/ServerMacros.scala
@@ -1,20 +1,29 @@
 package jsonrpc.server
 
 import jsonrpc.core.{Introspection, Method}
-import scala.quoted.{Expr, Quotes, Type, quotes}
+import scala.quoted.{Expr, Quotes, ToExpr, Type, quotes}
 
 object ServerMacros:
 
   inline def bind[T <: AnyRef](inline api: T): Unit = ${bind('api)}
 
-  private def bind[T <: AnyRef: Type](api: Expr[T])(using q: Quotes): Expr[Unit] =
+  private def bind[T <: AnyRef: Type](api: Expr[T])(using Quotes): Expr[Unit] =
     import quotes.reflect.*
     val apiTypeSymbol = TypeRepr.of[T].typeSymbol
     val apiMethods = Introspection.publicApiMethods(apiTypeSymbol, concrete = true)
     val result = apiMethods.map(methodDescription).mkString("\n")
-    '{
+    val typeParam = TypeRepr.of[List[List[String]]]
+    val methodName = apiMethods.find(_.arguments.flatten.size == 0).map(_.name).getOrElse("")
+    val call = Select.unique(api.asTerm, methodName).appliedToNone
+    val typedCall = Select.unique('{List}.asTerm, "apply").appliedToType(typeParam).appliedTo('{List.empty[List[String]]}.asTerm)
+    println(call.show)
+    println(typedCall.show)
+    val r = '{
       println(${Expr(result)})
+      println(${call.asExpr})
+      println(${typedCall.asExpr})
     }
+    r
 
   private def methodDescription(method: Method): String =
     val argumentLists = method.arguments.map { arguments =>
