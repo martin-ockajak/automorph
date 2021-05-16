dd90a2e828bca51d361778243d750a132bcd1126
Martin Ockajak <martin.ockajak@drion.ai>
2021-05-17 00:02:25 +0200

    Update Scala version: 3.0.0 Update dependency: upickle 1.3.13 Update dependency: scalatest 3.2.9 Update dependency: scalacheck-1-15 3.2.9.0 Remove dependency: zio 1.0.7

--- build.sbt
+++ build.sbt
@@ -2,15 +2,18 @@ lazy val root = project.in(file(".")).settings(
   name := "json-rpc",
   description := "JSON-RPC client & server",
   version := "0.1.0",
-  scalaVersion := "3.0.0-RC3",
+  scalaVersion := "3.0.0",
   libraryDependencies ++= Seq(
-    "com.lihaoyi" %% "upickle" % "1.3.12",
-    "io.circe" %% "circe-parser" % "0.14.0-M6",
-    "io.circe" %% "circe-generic" % "0.14.0-M6",
+    // JSON
+    "com.lihaoyi" %% "upickle" % "1.3.13",
     "com.fasterxml.jackson.module" % "jackson-module-scala_2.13" % "2.12.3",
-    "dev.zio" %% "zio" % "1.0.7",
-    "org.scalatest" %% "scalatest" % "3.2.8" % Test,
-    "org.scalatestplus" %% "scalacheck-1-15" % "3.2.8.0" % Test,
+
+    // Effect
+//    "dev.zio" % "zio" % "1.0.7",
+
+    // Test
+    "org.scalatest" %% "scalatest" % "3.2.9" % Test,
+    "org.scalatestplus" %% "scalacheck-1-15" % "3.2.9.0" % Test,
     "org.scala-lang" %% "scala3-tasty-inspector" % scalaVersion.value
   )
 )
--- src/main/scala/jsonrpc/effect/zio/ZioEffectContext.scala
+++ src/main/scala/jsonrpc/effect/zio/ZioEffectContext.scala
@@ -1,20 +1,20 @@
-package jsonrpc.effect.zio
-
-import java.io.{ByteArrayOutputStream, InputStream, OutputStream}
-import java.nio.ByteBuffer
-import jsonrpc.spi.EffectContext
-import scala.collection.immutable.ArraySeq
-import scala.util.{Failure, Success, Try}
-import zio.Task
-import zio.RIO
-
-final case class ZioEffectContext[Environment]()
-  extends EffectContext[[T] =>> RIO[Environment, T]]:
-
-  def pure[T](value: T): RIO[Environment, T] = RIO.succeed(value)
-
-  def map[T, R](value: RIO[Environment, T], function: T => R): RIO[Environment, R] = value.map(function)
-
-  def either[T](value: RIO[Environment, T]): RIO[Environment, Either[Throwable, T]] = value.either
-
-object ZioEffectContext
+//package jsonrpc.effect.zio
+//
+//import java.io.{ByteArrayOutputStream, InputStream, OutputStream}
+//import java.nio.ByteBuffer
+//import jsonrpc.spi.EffectContext
+//import scala.collection.immutable.ArraySeq
+//import scala.util.{Failure, Success, Try}
+//import zio.Task
+//import zio.RIO
+//
+//final case class ZioEffectContext[Environment]()
+//  extends EffectContext[[T] =>> RIO[Environment, T]]:
+//
+//  def pure[T](value: T): RIO[Environment, T] = RIO.succeed(value)
+//
+//  def map[T, R](value: RIO[Environment, T], function: T => R): RIO[Environment, R] = value.map(function)
+//
+//  def either[T](value: RIO[Environment, T]): RIO[Environment, Either[Throwable, T]] = value.either
+//
+//object ZioEffectContext
