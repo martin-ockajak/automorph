49d4c5542eeb4eaf0b7c6118e0c25987d3fa4e07
Martin Ockajak <martin.ockajak@drion.ai>
2021-05-18 14:34:56 +0200

    Cleanup

---
+++ src/main/scala/jsonrpc/json/upickle/UpickleMacros.scala
@@ -0,0 +1,40 @@
+package jsonrpc.json.upickle
+
+import jsonrpc.core.Reflection
+import jsonrpc.spi.{CallError, JsonContext, Message}
+import ujson.Value
+import ujson.Str
+import upickle.default.{Writer, Reader, ReadWriter, macroRW}
+import upickle.{Api, AttributeTagged}
+import scala.quoted.{Expr, Quotes, Type, quotes}
+
+object UpickleMacros:
+  inline def xencode[T](inline parser: AttributeTagged, inline value: T): Value = ${xencode[T]('parser, 'value)}
+
+  private def xencode[T: Type](parser: Expr[AttributeTagged], value: Expr[T])(using quotes: Quotes): Expr[Value] =
+    import ref.quotes.reflect.*
+
+    val ref = Reflection(quotes)
+    val parserTypeTree = TypeTree.of[AttributeTagged]
+    val parserMethods = ref.methods(parserTypeTree).filter(_.public)
+
+//    parserMethods.filter(_.resultType.show.contains("Writer")).foreach { method =>
+//      println(s"${method.name}: ${method.resultType.show}")
+//    }
+    //    val publicMethods = introspection.publicMethods(introspection.ref.TypeTree.of[AttributeTagged])
+    //    val publicDescription = publicMethods.map(method => s"${method.name} - ${method.flags}\n").mkString("\n")
+    //    println(publicDescription)
+
+    val valueType = TypeTree.of[T]
+    //    val call = reflection.callTerm(ref.term(api), "writeJs", List(valueType), List(List(ref.term(value))))
+    val writer = '{${parser}.StringWriter}
+    val callString = ref.callTerm(ref.term(parser), "writeJs",
+      List(TypeTree.of[String]), List(List(ref.term('{"test"})), List(ref.term(writer))))
+    //    println(call)
+    '{
+            ${callString.asExpr}.asInstanceOf[Value]
+    //      ${call.asExpr}
+    //      ${api}.writeJs[String]("test")(using ${api}.StringWriter)
+    //      ${api}.writeJs[T](${value})
+    //  Str("test")
+    }
