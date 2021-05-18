4e089f25db4e2e3158780472ee5fc865e7837bb4
Luigi Antognini <luigi.antognini@produs.ch>
2021-05-18 15:16:54 +0200

    macro/given/PDT example for bug report

--- src/main/scala/bugreport/MacroGivenResolution.scala
+++ src/main/scala/bugreport/MacroGivenResolution.scala
@@ -4,9 +4,9 @@ import scala.quoted.{Expr, Quotes, Type, quotes}
 
 inline def foo(inline x:Int, a:A): Int = ${foo('x, 'a)}
 
-private def foo(x: Expr[Int], a:Expr[A])(using quotes: Quotes): Expr[Int] = '{$a.plus($x)}
+private def foo(x: Expr[Int], a:Expr[A])(using quotes: Quotes): Expr[Int] = '{$a.increment($x)}
 
-inline def bar(inline x:Int, a:A): Int = a.plus(x)
+inline def bar(inline x:Int, a:A): Int = a.increment(x)
 
 
 // does NOT work if you move the next two lines inside class A
@@ -14,5 +14,5 @@ final case class Context()
 given Context = Context()
 
 class A:
-  def plus(i:Int)(using Context):Int = i+1
+  def increment(i:Int)(using Context):Int = i+1
 
