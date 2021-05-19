778ea9b53025e4952033c28c057589b8c0bab002
Martin Ockajak <martin.ockajak@drion.ai>
2021-05-19 19:47:02 +0200

    Refactoring

--- src/main/scala/jsonrpc/format/json/upickle/UpickleMacros.scala
+++ src/main/scala/jsonrpc/format/json/upickle/UpickleMacros.scala
@@ -5,9 +5,14 @@ import upickle.Api
 import scala.quoted.{Expr, Quotes, Type}
 
 object UpickleMacros:
-  inline def encode[Parser <: Api, T](inline parser: Parser, inline writer: Api#Writer[T], inline value: T): Value = ${encode[Parser, T]('parser, 'writer, 'value)}
+  inline def encode[Parser <: Api, T](inline parser: Parser, inline writer: Api#Writer[T], inline value: T): Value =
+    ${encode('parser, 'writer, 'value)}
 
-  private def encode[Parser <: Api: Type, T: Type](parser: Expr[Parser], writer: Expr[Api#Writer[T]], value: Expr[T])(using quotes: Quotes): Expr[Value] =
+  private def encode[Parser <: Api: Type, T: Type](
+    parser: Expr[Parser],
+    writer: Expr[Api#Writer[T]],
+    value: Expr[T]
+  )(using quotes: Quotes): Expr[Value] =
     '{
       val realParser = $parser
       realParser.writeJs(${value})(using ${writer}.asInstanceOf[realParser.Writer[T]])
