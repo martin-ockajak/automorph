0668e330ae71c16a460811c63d163987b8e9d62c
Martin Ockajak <martin.ockajak@drion.ai>
2021-05-20 01:58:41 +0200

    Documentation

--- src/main/scala/jsonrpc/ApplicationError.scala
+++
@@ -1,7 +0,0 @@
-package jsonrpc
-
-final case class ApplicationError[JsonValue](
-  message: String,
-  data: JsonValue,
-  cause: Throwable
-) extends RuntimeException(message, cause)
---
+++ src/main/scala/jsonrpc/JsonRpcError.scala
@@ -0,0 +1,16 @@
+package jsonrpc
+
+/**
+ * JSON-RPC error.
+ * An API call resulting in this exception translates into the specific JSON-RPC error details.
+ *
+ * @param message error description
+ * @param data additional error information
+ * @param cause the cause of this exception
+ * @tparam Node data format node representation type
+ */
+final case class JsonRpcError[Node](
+  message: String,
+  data: Node,
+  cause: Throwable
+) extends RuntimeException(message, cause)
