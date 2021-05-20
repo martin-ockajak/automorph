29e4788bbb10fa7737385b3ea70153d54d4f383e
Martin Ockajak <martin.ockajak@drion.ai>
2021-05-20 15:50:24 +0200

    Error description generator

---
+++ src/main/scala/jsonrpc/core/Errors.scala
@@ -0,0 +1,29 @@
+package jsonrpc.core
+
+object Errors:
+  val defaultMaxCauses: Int = 100
+  val messageDelimiter = ":"
+
+  /**
+   * Assemble concise error descriptions from specified throwable and specified number of its causes.
+   *
+   * @param throwable exception
+   * @param filter only include throwables satisfying this condition
+   * @param maxCauses maximum number of included exception causes
+   * @return error messages
+   */
+  def descriptions(
+    throwable: Throwable,
+    filter: Throwable => Boolean = _ => true,
+    maxCauses: Int = defaultMaxCauses
+  ): Seq[String] =
+    val errors = LazyList.iterate(Option(throwable))(_.flatMap { throwable =>
+      Option(throwable.getCause)
+    }).takeWhile(_.isDefined).flatten ++ throwable.getSuppressedn
+    val filteredErrors = errors.filter(filter).take(maxCauses)
+    filteredErrors.map { throwable =>
+      val className = throwable.getClass.getSimpleName
+      Option(throwable.getMessage).map { message =>
+        s"$className$messageDelimiter $message"
+      }.getOrElse(classNamen)
+    }
