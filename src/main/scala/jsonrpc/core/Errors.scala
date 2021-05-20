cf13add9d6cfbd22c3bc638ec00362239bf2d3cb
Martin Ockajak <martin.ockajak@drion.ai>
2021-05-20 18:35:45 +0200

    Cleanup

--- src/main/scala/jsonrpc/core/Errors.scala
+++ src/main/scala/jsonrpc/core/Errors.scala
@@ -7,7 +7,6 @@ object Errors:
   // private extensions to make Throwable more scala-like
   extension( throwable: Throwable)
     private def cause: Option[Throwable] = throwable.getCause.asOption
-    private def suppressedCauses: Seq[Throwable] = throwable.getSuppressed.toSeq
     private def message: Option[String] = throwable.getMessage.asOption.map(_.trim).filter(_.nonEmpty)
 
   /**
@@ -23,8 +22,7 @@ object Errors:
     filter: Throwable => Boolean = _ => true,
     maxCauses: Int = defaultMaxCauses
   ): Seq[String] =
-    val errors: Seq[Throwable] =
-      LazyList.iterate(throwable.asSome)(_.flatMap(_.cause)).takeWhile(_.isDefined).flatten ++ throwable.suppressedCauses
+    val errors = LazyList.iterate(throwable.asSome)(_.flatMap(_.cause)).takeWhile(_.isDefined).flatten
     val filteredErrors = errors.filter(filter).take(maxCauses)
     filteredErrors.map { throwable =>
       val className = throwable.getClass.getSimpleName
