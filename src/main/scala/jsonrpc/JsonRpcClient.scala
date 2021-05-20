c6ec359dbe1b592ff3fbc087182c365a19248a2e
Martin Ockajak <martin.ockajak@drion.ai>
2021-05-20 02:59:49 +0200

    Cleanup

--- src/main/scala/jsonrpc/JsonRpcClient.scala
+++ src/main/scala/jsonrpc/JsonRpcClient.scala
@@ -6,17 +6,17 @@ import jsonrpc.spi.{Effect, Codec}
 import scala.collection.immutable.ArraySeq
 
 final case class JsonRpcClient[Node, Outcome[_]](
-  jsonContext: Codec[Node],
-  effectContext: Effect[Outcome]
+  codec: Codec[Node],
+  effect: Effect[Outcome]
 ):
   def call[Result](method: String, arguments: Seq[Any]): Outcome[Result] = ???
 
   def call[Result](method: String, arguments: Map[String, Any]): Outcome[Result] = ???
 
   def notify(method: String, arguments: Seq[Any]): Outcome[Unit] =
-    effectContext.pure(())
+    effect.pure(())
 
   def notify(method: String, arguments: Map[String, Any]): Outcome[Unit] =
-    effectContext.pure(())
+    effect.pure(())
 
   def api[T]: T = ???
--- src/main/scala/jsonrpc/JsonRpcServer.scala
+++ src/main/scala/jsonrpc/JsonRpcServer.scala
@@ -8,8 +8,8 @@ import jsonrpc.spi.{Codec, Effect}
 import scala.collection.immutable.ArraySeq
 
 final case class JsonRpcServer[Node, Outcome[_]](
-  jsonContext: Codec[Node],
-  effectContext: Effect[Outcome]
+  codec: Codec[Node],
+  effect: Effect[Outcome]
 ):
   private val bufferSize = 4096
 
@@ -17,14 +17,14 @@ final case class JsonRpcServer[Node, Outcome[_]](
     ServerMacros.bind(api)
 
   def process(request: ArraySeq.ofByte): Outcome[ArraySeq.ofByte] =
-    effectContext.pure(request)
+    effect.pure(request)
 
   def process(request: ByteBuffer): Outcome[ByteBuffer] =
-    effectContext.map(process(request.asArraySeq), response => {
+    effect.map(process(request.asArraySeq), response => {
       ByteBuffer.wrap(response.unsafeArray).nn
     })
 
   def process(request: InputStream): Outcome[InputStream] =
-    effectContext.map(process(request.asArraySeq(bufferSize)), response => {
+    effect.map(process(request.asArraySeq(bufferSize)), response => {
       ByteArrayInputStream(response.unsafeArray)
     })
