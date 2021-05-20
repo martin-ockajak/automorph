b1966ffd1cabc32e1cbb0ab6f0f2a78e788e320f
Luigi Antognini <luigi.antognini@produs.ch>
2021-05-20 18:47:25 +0200

    massaged Errors object ;)

--- src/main/scala/jsonrpc/core/Errors.scala
+++ src/main/scala/jsonrpc/core/Errors.scala
@@ -1,11 +1,12 @@
 package jsonrpc.core
+
 import jsonrpc.core.ValueOps.*
 
 object Errors:
   private val defaultMaxCauses: Int = 100
 
   // private extensions to make Throwable more scala-like
-  extension( throwable: Throwable)
+  extension (throwable: Throwable)
     private def cause: Option[Throwable] = throwable.getCause.asOption
     private def message: Option[String] = throwable.getMessage.asOption.map(_.trim).filter(_.nonEmpty)
 
@@ -22,11 +23,10 @@ object Errors:
     filter: Throwable => Boolean = _ => true,
     maxCauses: Int = defaultMaxCauses
   ): Seq[String] =
-    val errors = LazyList.iterate(throwable.asSome)(_.flatMap(_.cause)).takeWhile(_.isDefined).flatten
-    val filteredErrors = errors.filter(filter).take(maxCauses)
-    filteredErrors.map { throwable =>
-      val className = throwable.getClass.getSimpleName
-      throwable.message.map { message =>
-        s"$className: $message"
-      }.getOrElse(className)
-    }
+    LazyList.iterate(throwable.asSome)(_.flatMap(_.cause))
+      .takeWhile(_.isDefined).flatten.filter(filter).take(maxCauses).map { throwable =>
+        val className = throwable.getClass.getSimpleName
+        throwable.message.map { message =>
+          s"$className: $message"
+        }.getOrElse(className)
+      }
--- src/main/scala/jsonrpc/core/ValueOps.scala
+++ src/main/scala/jsonrpc/core/ValueOps.scala
@@ -3,9 +3,10 @@ package jsonrpc.core
 case object ValueOps:
 
   extension [T](value: T)
-    def asSome: Option[T] = Some(value)
+    def asSome: Option[T] = Some(value.nn)
+
     def asOption: Option[T] = Option(value)
 
-    def asLeft[R]: Either[T, R] = Left(value)
+    def asLeft[R]: Either[T, R] = Left(value.nn)
 
-    def asRight[L]: Either[L, T] = Right(value)
+    def asRight[L]: Either[L, T] = Right(value.nn)
