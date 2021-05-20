161d09dcacdadb0bb8f12769fe2843bdf8784e31
Luigi Antognini <luigi.antognini@produs.ch>
2021-05-20 18:05:07 +0200

    massaged Errors object ;)

--- src/main/scala/jsonrpc/core/Errors.scala
+++ src/main/scala/jsonrpc/core/Errors.scala
@@ -1,8 +1,17 @@
 package jsonrpc.core
+import jsonrpc.core.ValueOps.*
 
 object Errors:
-  val defaultMaxCauses: Int = 100
-  val messageDelimiter = ":"
+  private val defaultMaxCauses: Int = 100
+
+  // private extensions to make Throwable more scala-like
+  extension( throwable: Throwable)
+    private def cause: Option[Throwable] = throwable.getCause.asOptionFromNullable
+    private def suppressedCauses: Seq[Throwable] = throwable.getSuppressed.toSeq
+    private def message: Option[String] = throwable.getMessage.asOptionFromNullable.map(_.trim).flatMap{
+      case "" => None
+      case x => Some(x)
+    }
 
   /**
    * Assemble concise error descriptions from specified throwable and specified number of its causes.
@@ -17,13 +26,12 @@ object Errors:
     filter: Throwable => Boolean = _ => true,
     maxCauses: Int = defaultMaxCauses
   ): Seq[String] =
-    val errors = LazyList.iterate(Option(throwable))(_.flatMap { throwable =>
-      Option(throwable.getCause)
-    }).takeWhile(_.isDefined).flatten ++ throwable.getSuppressed
+    val errors: Seq[Throwable] =
+      LazyList.iterate(throwable.asSome)(_.flatMap(_.cause)).takeWhile(_.isDefined).flatten ++ throwable.suppressedCauses
     val filteredErrors = errors.filter(filter).take(maxCauses)
     filteredErrors.map { throwable =>
       val className = throwable.getClass.getSimpleName
-      Option(throwable.getMessage).map { message =>
-        s"$className$messageDelimiter $message"
+      throwable.message.map { msg =>
+        s"$className: $msg"
       }.getOrElse(className)
     }
--- src/main/scala/jsonrpc/core/ValueOps.scala
+++ src/main/scala/jsonrpc/core/ValueOps.scala
@@ -4,6 +4,7 @@ case object ValueOps:
 
   extension [T](value: T)
     def asSome: Option[T] = Some(value)
+    def asOptionFromNullable: Option[T] = Option(value)
 
     def asLeft[R]: Either[T, R] = Left(value)
 
