c45650c7cea09960e38bcd23881b9412757f3584
Martin Ockajak <martin.ockajak@drion.ai>
2021-05-20 03:49:18 +0200

    JSON-RPC client prototype

--- src/main/scala/jsonrpc/JsonRpcClient.scala
+++ src/main/scala/jsonrpc/JsonRpcClient.scala
@@ -1,35 +1,56 @@
 package jsonrpc
 
-import java.io.{InputStream, OutputStream}
-import java.nio.ByteBuffer
-import jsonrpc.spi.{Effect, Codec}
+import jsonrpc.core.Protocol
+import jsonrpc.spi.{Effect, CallError, Codec}
 import scala.collection.immutable.ArraySeq
-import scala.util.Try
 
 /**
  * JSON-RPC client.
  *
  * @param codec hierarchical data format codec plugin
  * @param effect computation effect system plugin
+ * @param transport message transport
  * @tparam Node data format node representation type
  * @tparam Outcome computation outcome effect type
  */
 final case class JsonRpcClient[Node, Outcome[_]](
   codec: Codec[Node],
-  effect: Effect[Outcome]
+  effect: Effect[Outcome],
+  transport: JsonRpcTransport[Outcome]
 ):
-  def call[R](method: String, arguments: Seq[Any]): Outcome[R] = ???
+  def call[R](method: String, arguments: Seq[Any]): Outcome[R] =
+    call(method, encodeArguments(arguments))
 
-  def call[R](method: String, arguments: Map[String, Any]): Outcome[R] = ???
+  def call[R](method: String, arguments: Map[String, Any]): Outcome[R] =
+    call(method, encodeArguments(arguments))
 
-  def notify(method: String, arguments: Seq[Any]): Outcome[Unit] = ???
+  def notify(method: String, arguments: Seq[Any]): Outcome[Unit] =
+    notify(method, encodeArguments(arguments))
 
-  def notify(method: String, arguments: Map[String, Any]): Outcome[Unit] = ???
+  def notify(method: String, arguments: Map[String, Any]): Outcome[Unit] =
+    notify(method, encodeArguments(arguments))
 
   def api[T]: T = ???
 
-  def callRequest(method: String, arguments: Seq[Any]): ArraySeq.ofByte = ???
+  private def encodeArguments(arguments: Seq[Any]): Node = ???
 
-  def notifyRequest(method: String, arguments: Seq[Any]): ArraySeq.ofByte = ???
+  private def encodeArguments(arguments: Map[String, Any]): Node = ???
 
-  def responseResult[R](response: ArraySeq.ofByte): Try[R] = ???
+  private def decodeResult[R](value: Either[CallError[Node], Node]): Either[Throwable, R] = ???
+
+  private def requestMessage(id: Boolean, method: String, arguments: Node): ArraySeq.ofByte = ???
+
+  private def responseMessage(response: ArraySeq.ofByte): Protocol.Response[Node] = ???
+
+  private def call[R](method: String, arguments: Node): Outcome[R] =
+    val request = requestMessage(id = true, method, arguments)
+    effect.map(transport.call(request), response => {
+      val message = responseMessage(response)
+      decodeResult(message.value) match
+        case Left(error) => throw error
+        case Right(result) => result
+    })
+
+  private def notify[R](method: String, arguments: Node): Outcome[Unit] =
+    val request = requestMessage(id = false, method, arguments)
+    effect.map(transport.call(request), _ => ())
---
+++ src/main/scala/jsonrpc/JsonRpcTransport.scala
@@ -0,0 +1,14 @@
+package jsonrpc
+
+import jsonrpc.spi.Effect
+import scala.collection.immutable.ArraySeq
+
+/**
+ * JSON-RPC transport.
+ *
+ * @tparam Outcome computation outcome effect type
+ */
+trait JsonRpcTransport[Outcome[_]]:
+  def call(request: ArraySeq.ofByte): Outcome[ArraySeq.ofByte]
+
+  def notify(request: ArraySeq.ofByte): Outcome[ArraySeq.ofByte]
