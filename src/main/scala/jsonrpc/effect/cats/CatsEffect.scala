b4c8c8681afe2ead5e543cc5e5b191b20cd2f45b
Martin Ockajak <martin.ockajak@drion.ai>
2021-05-20 14:40:29 +0200

    Format

--- src/main/scala/jsonrpc/effect/cats/CatsEffect.scala
+++ src/main/scala/jsonrpc/effect/cats/CatsEffect.scala
@@ -13,11 +13,8 @@ import jsonrpc.spi.Effect
 final case class CatsEffect[Environment]()
   extends Effect[IO]:
 
-  def pure[T](value: T): IO[T] =
-    IO.pure(value)
+  def pure[T](value: T): IO[T] = IO.pure(value)
 
-  def map[T, R](value: IO[T], function: T => R): IO[R] =
-    value.map(function)
+  def map[T, R](value: IO[T], function: T => R): IO[R] = value.map(function)
 
-  def either[T](value: IO[T]): IO[Either[Throwable, T]] =
-    value.attempt
+  def either[T](value: IO[T]): IO[Either[Throwable, T]] = value.attempt
--- src/main/scala/jsonrpc/effect/monix/MonixEffect.scala
+++ src/main/scala/jsonrpc/effect/monix/MonixEffect.scala
@@ -13,11 +13,8 @@ import monix.eval.Task
 final case class MonixEffect[Environment]()
   extends Effect[Task]:
 
-  def pure[T](value: T): Task[T] =
-    Task.pure(value)
+  def pure[T](value: T): Task[T] = Task.pure(value)
 
-  def map[T, R](effect: Task[T], function: T => R): Task[R] =
-    effect.map(function)
+  def map[T, R](effect: Task[T], function: T => R): Task[R] = effect.map(function)
 
-  def either[T](value: Task[T]): Task[Either[Throwable, T]] =
-    value.attempt
+  def either[T](value: Task[T]): Task[Either[Throwable, T]] = value.attempt
