51259ee63ff41034f54a18411d367316d80e5346
Martin Ockajak <martin.ockajak@drion.ai>
2021-05-23 19:42:47 +0200

    Cleanup

--- src/test/scala/jsonrpc/ServerSpec.scala
+++ src/test/scala/jsonrpc/ServerSpec.scala
@@ -105,7 +105,7 @@ class ServerSpec extends BaseSpec:
 //        println(jsonContext.decode[String](jsonContext.encode("test")))
 //        val messageJson = jsonContext.serialize(upickleMessage)
 //        println(messageJson)
-//        println(jsonContext.derialize(messageJson))
+//        println(jsonContext.deserialize(messageJson))
 //        println(jsonContext.format(upickleMessage))
       }
     }
