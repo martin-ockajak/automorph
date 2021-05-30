224197330a640e4c24fea863202fa29a72631e73
Martin Ockajak <martin.ockajak@drion.ai>
2021-05-30 13:48:03 +0200

    Binding function generation

--- src/main/scala/jsonrpc/handler/HandlerMacros.scala
+++ src/main/scala/jsonrpc/handler/HandlerMacros.scala
@@ -85,7 +85,7 @@ object HandlerMacros:
 
     // Generate API method handles including wrapper functions consuming and product Node values
     val methodHandles = Expr.ofSeq(apiMethods.map { method =>
-      generateMethodHandle[Node, CodecType, Outcome, Context](ref, method, codec, effect)
+      generateMethodHandle[Node, CodecType, Outcome, Context, ApiType](ref, method, codec, effect, api)
     })
 
     // Generate function call using a type parameter
@@ -134,15 +134,16 @@ object HandlerMacros:
     }
     methods
 
-  private def generateMethodHandle[Node: Type, CodecType <: Codec[Node]: Type, Outcome[_]: Type, Context: Type](
+  private def generateMethodHandle[Node: Type, CodecType <: Codec[Node]: Type, Outcome[_]: Type, Context: Type, ApiType: Type](
     ref: Reflection,
     method: ref.QuotedMethod,
     codec: Expr[CodecType],
-    effect: Expr[Effect[Outcome]]
+    effect: Expr[Effect[Outcome]],
+    api: Expr[ApiType]
   ): Expr[(String, MethodHandle[Node, Outcome, Context])] =
     given Quotes = ref.quotes
     val liftedMethod = method.lift
-    val function = generateFunction[Node, CodecType, Outcome, Context](ref, method, codec, effect)
+    val function = generateFunction[Node, CodecType, Outcome, Context, ApiType](ref, method, codec, effect, api)
     val name = Expr(liftedMethod.name)
     val resultType = Expr(liftedMethod.resultType)
     val paramNames = Expr(liftedMethod.params.flatMap(_.map(_.name)))
@@ -151,11 +152,12 @@ object HandlerMacros:
       $name -> MethodHandle($function, $name, $resultType, $paramNames, $paramTypes)
     }
 
-  private def generateFunction[Node: Type, CodecType <: Codec[Node]: Type, Outcome[_]: Type, Context: Type](
+  private def generateFunction[Node: Type, CodecType <: Codec[Node]: Type, Outcome[_]: Type, Context: Type, ApiType: Type](
     ref: Reflection,
     method: ref.QuotedMethod,
     codec: Expr[CodecType],
-    effect: Expr[Effect[Outcome]]
+    effect: Expr[Effect[Outcome]],
+    api: Expr[ApiType]
   ): Expr[(Seq[Node], Option[Context]) => Outcome[Node]] =
     import ref.quotes.reflect.{asTerm, Lambda, MethodType, Printer, Symbol, Term, TypeRepr}
     given Quotes = ref.quotes
@@ -163,27 +165,36 @@ object HandlerMacros:
     val argumentConverters = method.params.flatMap(_.map { param =>
       Lambda(
         Symbol.spliceOwner,
-        MethodType(List("argument"))(_ => List(TypeRepr.of[Node]), _ => param.dataType),
+        MethodType(List("nodeAargument"))(_ => List(TypeRepr.of[Node]), _ => param.dataType),
         (symbol, args) => ref.callTerm(codec.asTerm, "decode", List(param.dataType), List(args.asInstanceOf[List[Term]]))
-      )
+      ).asExpr
     })
     val resultConverter = Lambda(
       Symbol.spliceOwner,
       MethodType(List("result"))(_ => List(method.resultType), _ => TypeRepr.of[Node]),
       (symbol, args) => ref.callTerm(codec.asTerm, "encode", List(method.resultType), List(args.asInstanceOf[List[Term]]))
-    )
+    ).asExpr
+//    val methodCaller = Lambda(
+//      Symbol.spliceOwner,
+//      MethodType(List("arguments"))(_ => List(method.resultType), _ => method.resultType),
+//      (symbol, args) => ref.callTerm(api.asTerm, method.name, List(method.resultType), List(args.asInstanceOf[List[Term]]))
+//    ).asExpr
 
     // Debug prints
     println(method.name)
     argumentConverters.foreach { converter =>
-      println(s"  ${converter.show(using Printer.TreeCode)}")
+      println(s"  ${converter.asTerm.show(using Printer.TreeCode)}")
     }
-    println(s"  ${resultConverter.show(using Printer.TreeCode)}")
+    println(s"  ${resultConverter.asTerm.show(using Printer.TreeCode)}")
     println()
 
     val function = '{
-      (arguments: Seq[Node], context: Option[Context]) =>
-        $effect.pure(arguments.head)
+      (nodeArguments: Seq[Node], context: Option[Context]) =>
+        val resultConv = ${resultConverter}
+        val arguments = nodeArguments.zip(${Expr.ofSeq(argumentConverters)}).map { (argument, converter) =>
+          converter.asInstanceOf[Node => Any](argument)
+        }
+        $effect.pure(nodeArguments.head)
     }
 //    println(function.asTerm)
 //  Inlined(List(DefDef($anonfun,List(List(ValDef(argument,TypeTree - argument,EmptyTree))),TypeTree - Node,Block(List(), callTerm(codec.asTerm, "decode", List(param.dataType), List(Select(Ident(argument))))))
