2b350870f18312e5c84173ac7862991ef7320af3
Martin Ockajak <martin.ockajak@drion.ai>
2021-06-05 15:17:31 +0200

    Tests

--- core/src/test/scala/jsonrpc/HandlerSpec.scala
+++ core/src/test/scala/jsonrpc/HandlerSpec.scala
@@ -6,9 +6,10 @@ import jsonrpc.codec.json.dummy.DummyJsonCodec
 import jsonrpc.codec.json.upickle.UpickleJsonCodec
 import jsonrpc.effect.standard.{FutureEffect, NoEffect}
 import jsonrpc.transport.local.HandlerTransport
-import jsonrpc.{ApiImpl, JsonRpcHandler, SimpleApi}
+import jsonrpc.{ApiImpl, Enum, JsonRpcHandler, Record, SimpleApi, Structure}
 import scala.collection.immutable.ArraySeq
 import scala.concurrent.ExecutionContext.Implicits.global
+import upickle.AttributeTagged
 
 class HandlerSpec extends BaseSpec:
 
@@ -19,9 +20,9 @@ class HandlerSpec extends BaseSpec:
         val simpleApi = SimpleApi()
         JsonRpcHandler[String, DummyJsonCodec, NoEffect.Identity, String](DummyJsonCodec(), NoEffect()).bind(simpleApi)
         val handler = JsonRpcHandler[String, DummyJsonCodec, NoEffect.Identity, NoContext](DummyJsonCodec(), NoEffect())
-        val futureHandler = JsonRpcHandler.basic(DummyJsonCodec(), FutureEffect()).bind(api)
-//        val futureHandler = JsonRpcHandler(UpickleJsonCodec(), FutureEffect()).bind(api)
-//        handler.processRequest(ArraySeq.ofByte(Array.empty[Byte]))
+//        val futureHandler = JsonRpcHandler.basic(DummyJsonCodec(), FutureEffect()).bind(api)
+        val futureHandler = JsonRpcHandler.basic(UpickleJsonCodec(JsonPickler), FutureEffect()).bind(api)
+//        futureHandler.processRequest(ArraySeq.ofByte(Array.empty[Byte]))
 
         val transport = HandlerTransport(handler, NoEffect())
         val client = JsonRpcClient[String, DummyJsonCodec, NoEffect.Identity, NoContext](DummyJsonCodec(), NoEffect(), transport)
@@ -33,3 +34,12 @@ class HandlerSpec extends BaseSpec:
       }
     }
   }
+
+object JsonPickler extends AttributeTagged:
+
+  given ReadWriter[Enum] = readwriter[Int].bimap[Enum](
+    value => value.ordinal,
+    number => Enum.fromOrdinal(number)
+  )
+  given ReadWriter[Structure] = macroRW
+  given ReadWriter[Record] = macroRW
