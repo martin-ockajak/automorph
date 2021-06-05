dd235eb75107f1bee669c0ce2d9aeff082750fe7
Martin Ockajak <martin.ockajak@drion.ai>
2021-06-05 23:41:12 +0200

    Undertow HTTP server

--- server/undertow/src/test/scala/jsonrpc/server/http/cask/CaskServerSpec.scala
+++ server/undertow/src/test/scala/jsonrpc/server/http/cask/CaskServerSpec.scala
@@ -1,34 +1,31 @@
-//package jsonrpc.server.http.cask
-//
-//import base.BaseSpec
-//import io.undertow.server.HttpHandler
-//import io.undertow.Handlers
-//import io.undertow.server.handlers.BlockingHandler
-//import jsonrpc.JsonRpcHandler
-//import jsonrpc.backend.standard.NoBackend
-//import jsonrpc.codec.json.upickle.UpickleJsonCodec
-//import jsonrpc.http.undertow.UndertowJsonRpcHandler
-//import scala.language.adhocExtensions
-//
-//class CaskServerSpec extends BaseSpec {
-//
-//}
-//
-//object Api:
-//  def test(value: String): String = value
-//
-//object CaskServer extends cask.MainRoutes:
-//  val apiPath = "/api"
-//
-//  override def defaultHandler =
-//    val httpHandler = UndertowJsonRpcHandler(
-//      JsonRpcHandler(UpickleJsonCodec(), NoBackend()).bind(Api),
-//      _ => ()
-//    )
-//    val pathHandler = Handlers.path(super.defaultHandler).addPrefixPath(apiPath, httpHandler)
-//    BlockingHandler(pathHandler)
-//
-//  @cask.get("/")
-//  def hello() = "Hello World!"
-//
-//  initialize()
+package jsonrpc.server.http.cask
+
+import base.BaseSpec
+import io.undertow.server.HttpHandler
+import io.undertow.Handlers
+import io.undertow.server.handlers.BlockingHandler
+import jsonrpc.JsonRpcHandler
+import jsonrpc.backend.standard.NoBackend
+import jsonrpc.codec.json.upickle.UpickleJsonCodec
+import jsonrpc.http.undertow.UndertowJsonRpcHandler
+import scala.language.adhocExtensions
+
+class CaskServerSpec extends BaseSpec {
+
+}
+
+object Api:
+  def test(value: String): String = value
+
+object CaskServer extends cask.MainRoutes:
+  val apiPath = "/api"
+
+  override def defaultHandler =
+    val httpHandler = UndertowJsonRpcHandler(
+      JsonRpcHandler(UpickleJsonCodec(), NoBackend()).bind(Api),
+      _ => ()
+    )
+    val pathHandler = Handlers.path(super.defaultHandler).addPrefixPath(apiPath, httpHandler)
+    BlockingHandler(pathHandler)
+
+  initialize()
