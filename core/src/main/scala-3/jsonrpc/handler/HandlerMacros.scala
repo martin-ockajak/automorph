05dfc5d9675f04bff2725caa02aae944e882bd53
Martin Ockajak <martin.ockajak@drion.ai>
2021-06-06 22:40:17 +0200

    Refactoring

--- core/src/main/scala-3/jsonrpc/handler/HandlerMacros.scala
+++ core/src/main/scala-3/jsonrpc/handler/HandlerMacros.scala
@@ -7,27 +7,6 @@ import jsonrpc.util.{Method, Reflection}
 import scala.collection.immutable.ArraySeq
 import scala.quoted.{quotes, Expr, Quotes, Type}
 
-/**
- * Bound API method handle.
- *
- * @param function binding function wrapping the bound method
- * @param name method name
- * @param resultType result type
- * @param paramNames parameter names
- * @param parameterTypes paramter types
- * @tparam Node message format node representation type
- * @tparam Effect effect type
- * @tparam Context request context type
- */
-final case class MethodHandle[Node, Effect[_], Context](
-  function: (Seq[Node], Context) => Effect[Node],
-  name: String,
-  resultType: String,
-  paramNames: Seq[String],
-  parameterTypes: Seq[String],
-  usesContext: Boolean
-)
-
 case object HandlerMacros:
 
   private val debugProperty = "jsonrpc.macro.debug"
---
+++ core/src/main/scala/jsonrpc/handler/MethodHandle.scala
@@ -0,0 +1,22 @@
+package jsonrpc.handler
+
+/**
+ * Bound API method handle.
+ *
+ * @param function binding function wrapping the bound method
+ * @param name method name
+ * @param resultType result type
+ * @param paramNames parameter names
+ * @param parameterTypes paramter types
+ * @tparam Node message format node representation type
+ * @tparam Effect effect type
+ * @tparam Context request context type
+ */
+final case class MethodHandle[Node, Effect[_], Context](
+  function: (Seq[Node], Context) => Effect[Node],
+  name: String,
+  resultType: String,
+  paramNames: Seq[String],
+  parameterTypes: Seq[String],
+  usesContext: Boolean
+)
