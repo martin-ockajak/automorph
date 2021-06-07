bbaef7d425a3cfc72fd00af1cb00e50873f3b149
Luigi Antognini <luigi.antognini@produs.ch>
2021-06-08 01:54:45 +0200

    added ClientProxy

--- core/src/main/scala/jsonrpc/ClientProxy.scala
+++ core/src/main/scala/jsonrpc/ClientProxy.scala
@@ -2,14 +2,14 @@ package jsonrpc
 import java.lang.reflect.Proxy
 import scala.reflect.ClassTag
 
-final case class Callback(methodName: String, params: Seq[Any])
+final case class Invocation(methodName: String, params: Seq[Any])
 
 final case class ClientProxy():
-  def instanceFor[T: ClassTag](f: Callback => Any):T =
+  def instanceFor[T: ClassTag](callback: Invocation => Any):T =
     Proxy.newProxyInstance(
       this.getClass.getClassLoader,
       Array( summon[ClassTag[T]].runtimeClass),
-      ( proxy, method, params) => f( Callback( method.getName, params.toSeq))
+      ( proxy, method, params) => callback( Invocation( method.getName, params.toSeq))
     ).asInstanceOf[T]
 
 
@@ -22,10 +22,10 @@ case object ClientProxy:
       def sleep(seconds:Int): String
 
     val proxy = ClientProxy().instanceFor[Dog]{
-      case Callback( "bark", Seq(i)) => s"barking for $i seconds"
-      case Callback( "bark", Seq(i, happy)) => s"barking for $i seconds, happy = $happy"
-      case Callback( "sleep", Seq(i)) => s"sleeping for $i seconds"
-      case Callback(  method, params) => sys.error(s"unknown method $method(${params.mkString(", ")})")
+      case Invocation( "bark", Seq(i)) => s"barking for $i seconds"
+      case Invocation( "bark", Seq(i, happy)) => s"barking for $i seconds, happy = $happy"
+      case Invocation( "sleep", Seq(i)) => s"sleeping for $i seconds"
+      case Invocation(  method, params) => sys.error(s"unknown method $method(${params.mkString(", ")})")
     }
 
     extension( x:Any) def println() = Console.println(x)
