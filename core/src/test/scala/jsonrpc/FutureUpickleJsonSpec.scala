8ccc4470f78874d72ea1b407f32ce164e2d51a8c
Martin Ockajak <martin.ockajak@drion.ai>
2021-06-08 15:11:12 +0200

    Cleanup

--- core/src/test/scala/jsonrpc/FutureUpickleJsonSpec.scala
+++ core/src/test/scala/jsonrpc/FutureUpickleJsonSpec.scala
@@ -1,21 +1,19 @@
 package jsonrpc
 
 import jsonrpc.FutureUpickleJsonSpec.Effect
-import jsonrpc.ReadWriters
 import jsonrpc.UpickleJsonSpec.{CodecType, Node}
 import jsonrpc.backend.standard.FutureBackend
-import jsonrpc.codec.json.UpickleJsonCodec
-import jsonrpc.spi.{Backend, Codec, Transport}
+import jsonrpc.spi.Backend
 import jsonrpc.transport.local.HandlerTransport
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class EffectUpickleJsonSpec extends CoreSpec[Node, CodecType, Effect]:
+class EffectUpickleJsonSpec extends UpickleJsonSpec[Effect]:
 
   override def backend: Backend[Effect] = FutureBackend()
 
   lazy val theClient =
-    val codec = UpickleJsonCodec(ReadWriters)
+//    val codec = UpickleJsonCodec(ReadWriters)
     val handler = Handler[Node, CodecType, Effect, Short](codec, backend)
       .bind(simpleApi).bind[ComplexApi[Effect]](complexApi)
     val transport = HandlerTransport[Node, CodecType, Effect, Short](handler, backend, 0)
--- core/src/test/scala/jsonrpc/UpickleJsonSpec.scala
+++ core/src/test/scala/jsonrpc/UpickleJsonSpec.scala
@@ -1,25 +1,22 @@
 package jsonrpc
 
-import base.BaseSpec
-import jsonrpc.UpickleJsonSpec.{CodecType, Node}
+import jsonrpc.UpickleJsonSpec.{CodecType, Node, ReadWriters}
 import jsonrpc.codec.json.UpickleJsonCodec
-import jsonrpc.spi.Codec
-import jsonrpc.{ComplexApi, Enum, Record, SimpleApiImpl, Structure}
 import ujson.Value
 import upickle.AttributeTagged
 
-//class UpickleJsonSpec[Effect[_]] extends CoreSpec[Value, CodecType, Effect]:
-//  def codec: CodecType = UpickleJsonCodec(ReadWriters)
+trait UpickleJsonSpec[Effect[_]] extends CoreSpec[Value, CodecType, Effect]:
+  def codec: CodecType = UpickleJsonCodec(ReadWriters)
 
 object UpickleJsonSpec:
   type Node = Value
   type CodecType = UpickleJsonCodec[ReadWriters.type]
 
-object ReadWriters extends AttributeTagged:
+  object ReadWriters extends AttributeTagged:
 
-  given ReadWriter[Enum] = readwriter[Int].bimap[Enum](
-    value => value.ordinal,
-    number => Enum.fromOrdinal(number)
-  )
-  given ReadWriter[Structure] = macroRW
-  given ReadWriter[Record] = macroRW
+    given ReadWriter[Enum] = readwriter[Int].bimap[Enum](
+      value => value.ordinal,
+      number => Enum.fromOrdinal(number)
+    )
+    given ReadWriter[Structure] = macroRW
+    given ReadWriter[Record] = macroRW
