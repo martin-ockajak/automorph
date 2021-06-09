4e48466557e653f6d833542d7740574e05f0fc26
Martin Ockajak <martin.ockajak@drion.ai>
2021-06-09 17:02:44 +0200

    Refactoring

--- core/src/test/scala/jsonrpc/core/ClientHandlerSpec.scala
+++ core/src/test/scala/jsonrpc/core/ClientHandlerSpec.scala
@@ -32,45 +32,59 @@ trait ClientHandlerSpec[Node, CodecType <: Codec[Node], Effect[_]] extends BaseS
   )
 
   "" - {
-    "Static" - {
-      "Call" - {
-        "Simple API" - {
-          apiCombinations(simpleApiInstance, "Local" -> simpleApiLocal, "Remote" -> simpleApiRemote).foreach { case (bindings, apis) =>
-            bindings - {
-              "test" ignore {
-                val Seq(expected, result) = apis.map(api => run(api.test("test")))
-                expected.should(equal(result))
+    "Call" - {
+      "Named" - {
+        "Trait" - {
+          "Simple API" - {
+            apiCombinations(simpleApiInstance, "Local" -> simpleApiLocal, "Remote" -> simpleApiRemote).foreach { case (bindings, apis) =>
+              bindings - {
+                "test" ignore {
+                  val Seq(expected, result) = apis.map(api => run(api.test("test")))
+                  expected.should(equal(result))
+                }
+              }
+            }
+          }
+          "Complex API" - {
+            apiCombinations(complexApiInstance, "Local" -> complexApiLocal, "Remote" -> complexApiRemote).foreach { case (binding, apis) =>
+              binding - {
+                "method0" ignore {
+                  val Seq(expected, result) = apis.map(api => run(api.method0()))
+                  expected.should(equal(result))
+                }
               }
             }
           }
         }
-        "Complex API" - {
-          apiCombinations(complexApiInstance, "Local" -> complexApiLocal, "Remote" -> complexApiRemote).foreach { case (binding, apis) =>
-            binding - {
-              "method0" ignore {
-                val Seq(expected, result) = apis.map(api => run(api.method0()))
-                expected.should(equal(result))
-              }
-            }
+        "Case class" - {
+          "Simple API" in {
+//            client.bind("test").call[Arguments, Int](Arguments("test", 1))(using 0)
+          }
+        }
+        "Tuple" - {
+          "Simple API" in {
+//            client.callByName[Int]("test")("a", "b")(1, 2, 3)(using 0)
+//            client.bind("test").parameters("a", "b").call[Int](1, 2, 3)(using 0)
+//            val x = client.callByName[Int]("test")("a", "b")
+//            val y = x(1, 2, 3)(using 0)
+//            y(0)
+          }
+        }
+      }
+      "Positional" - {
+        "Tuple" - {
+          "Simple API" in {
+//            client.callByPosition[Int]("test")(1, 2, 3)(using 0)
           }
         }
       }
     }
-    "Dynamic" - {
-      "Call" - {
-        "Simple API" in {
-          //          client.callByName[Int]("test")("a", "b")(1, 2, 3)(using 0)
-          //          client.bind("test").parameters("a", "b").call[Int](1, 2, 3)(using 0)
-          //          client.bind("test").call[Arguments, Int](Arguments("test", 1))(using 0)
-          //          val x = client.callByName[Int]("test")("a", "b")
-          //          val y = x(1, 2, 3)(using 0)
-          //          y(0)
-        }
-      }
-      "Notify" - {
-        "Simple API" in {
+    "Notify" - {
+      "Named" ignore {
 
-        }
+      }
+      "Positional" ignore {
+
       }
     }
   }
