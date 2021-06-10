7f81f6bbe64786f18ae9a018a3f8afbf9c395d4c
Martin Ockajak <martin.ockajak@drion.ai>
2021-06-10 18:54:10 +0200

    Source statistics script

--- tools/source-stats.sh
+++ tools/source-stats.sh
@@ -5,7 +5,11 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
 cd ${SCRIPT_DIR}/..
 
-echo "Lines of code:"
+echo -e "\n### Files"
+echo "* Main: `find  . -name '*.scala' -type f | grep src/main/ | wc -l`"
+echo "* Test: `find  . -name '*.scala' -type f | grep src/test/ | wc -l`"
+echo "* Build: `find . \( -name '*.sbt' \) -type f | wc -l`"
+echo -e "\n### Lines"
 echo "* Main: `find  . -name '*.scala' -type f | grep src/main/ | xargs cat | wc -l`"
 echo "* Test: `find  . -name '*.scala' -type f | grep src/test/ | xargs cat | wc -l`"
 echo "* Build: `find . \( -name '*.sbt' \) -type f | xargs cat | wc -l`"
