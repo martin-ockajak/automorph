ad963d57caa23e9014a63c5ee70fc5c86fd5e14e
Martin Ockajak <martin.ockajak@drion.ai>
2021-06-11 05:06:40 +0200

    Tests

---
+++ core/src/test/scala/jsonrpc/backend/standard/TryCirceJsonSpec.scala
@@ -0,0 +1,26 @@
+//package jsonrpc.backend.standard
+//
+//import jsonrpc.backend.standard.FutureBackend
+//import jsonrpc.client.UnnamedBinding
+//import jsonrpc.codec.json.UpickleJsonSpec
+//import jsonrpc.spi.Backend
+//import jsonrpc.transport.local.HandlerTransport
+//import jsonrpc.{Client, ComplexApi, InvalidApi, SimpleApi}
+//import scala.concurrent.ExecutionContext.Implicits.global
+//import scala.concurrent.Future
+//
+//class TryCirceJsonSpec extends UpickleJsonSpec:
+//  type Effect[T] = Future[T]
+//
+//  override def backend: Backend[Effect] = FutureBackend()
+//
+//  override def run[T](effect: Effect[T]): T = await(effect)
+//
+//  override def client: Client[Node, CodecType, Effect, Short, UnnamedBinding[Node, CodecType, Effect, Short]] =
+//    Client(codec, backend, handlerTransport)
+//
+//  override def simpleApis: Seq[SimpleApi[Effect]] = clients.map(_.bind[SimpleApi[Effect]])
+//
+//  override def complexApis: Seq[ComplexApi[Effect]] = clients.map(_.bind[ComplexApi[Effect]])
+//
+//  override def invalidApis: Seq[InvalidApi[Effect]] = clients.map(_.bind[InvalidApi[Effect]])
