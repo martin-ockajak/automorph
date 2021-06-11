e7cf60cbfa677d0ce69bea407bfcbd761f0d0977
Martin Ockajak <martin.ockajak@drion.ai>
2021-06-11 05:07:16 +0200

    Tests

--- core/src/test/scala/jsonrpc/backend/standard/TryCirceJsonSpec.scala
+++ core/src/test/scala/jsonrpc/backend/standard/TryCirceJsonSpec.scala
@@ -1,20 +1,20 @@
 //package jsonrpc.backend.standard
 //
-//import jsonrpc.backend.standard.FutureBackend
+//import jsonrpc.backend.standard.TryBackend
 //import jsonrpc.client.UnnamedBinding
 //import jsonrpc.codec.json.UpickleJsonSpec
 //import jsonrpc.spi.Backend
 //import jsonrpc.transport.local.HandlerTransport
 //import jsonrpc.{Client, ComplexApi, InvalidApi, SimpleApi}
 //import scala.concurrent.ExecutionContext.Implicits.global
-//import scala.concurrent.Future
+//import scala.util.Try
 //
 //class TryCirceJsonSpec extends UpickleJsonSpec:
-//  type Effect[T] = Future[T]
+//  type Effect[T] = Try[T]
 //
-//  override def backend: Backend[Effect] = FutureBackend()
+//  override def backend: Backend[Effect] = TryBackend()
 //
-//  override def run[T](effect: Effect[T]): T = await(effect)
+//  override def run[T](effect: Effect[T]): T = effect.get
 //
 //  override def client: Client[Node, CodecType, Effect, Short, UnnamedBinding[Node, CodecType, Effect, Short]] =
 //    Client(codec, backend, handlerTransport)
