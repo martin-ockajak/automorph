62d2c2cff0553721796749f169455feb7ab4f216
Martin Ockajak <martin.ockajak@drion.ai>
2021-06-13 01:34:42 +0200

    Scala 2

--- test/core/src/main/scala/jsonrpc/codec/json/UpickleJsonSpec.scala
+++ test/core/src/main/scala/jsonrpc/codec/json/UpickleJsonSpec.scala
@@ -21,10 +21,10 @@ trait UpickleJsonSpec extends ClientHandlerSpec {
 
 object UpickleJsonSpec extends UpickleCustom {
 
-  given ReadWriter[Enum] = readwriter[Int].bimap[Enum](
+  implicit def enumRw: ReadWriter[Enum] = readwriter[Int].bimap[Enum](
     value => value.ordinal,
     number => Enum.fromOrdinal(number)
   )
-  given ReadWriter[Structure] = macroRW
-  given ReadWriter[Record] = macroRW
+  implicit def structureRw: ReadWriter[Structure] = macroRW
+  implicit def recordRw: ReadWriter[Record] = macroRW
 }
