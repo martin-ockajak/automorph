294c9f15c1bf9ccac23d6fabf35e520720d1c363
Martin Ockajak <martin.ockajak@drion.ai>
2021-06-13 03:21:02 +0200

    Scala 2

---
+++ core/src/main/scala-3/jsonrpc/util/Descriptors.scala
@@ -0,0 +1,48 @@
+package jsonrpc.util
+
+final case class Parameter(
+  name: String,
+  dataType: String,
+  contextual: Boolean
+)
+
+final case class TypeParameter(
+  name: String,
+  bounds: String
+)
+
+final case class Method(
+  name: String,
+  resultType: String,
+  parameters: Seq[Seq[Parameter]],
+  typeParameters: Seq[TypeParameter],
+  public: Boolean,
+  available: Boolean,
+  documentation: Option[String]
+) {
+
+  /** Method signature. */
+  lazy val signature: String = {
+    val typeParametersText = typeParameters.map { typeParameter =>
+      s"${typeParameter.name}"
+    } match
+      case Seq() => ""
+      case values => s"[${values.mkString(", ")}]"
+    val parametersText = parameters.map { parameters =>
+      s"(${
+        parameters.map { parameter =>
+          s"${parameter.name}: ${parameter.dataType}"
+        }.mkString(", ")
+      })"
+    }.mkString
+    s"$name$typeParametersText$parametersText: $resultType"
+  }
+}
+
+final case class Field(
+  name: String,
+  dataType: String,
+  public: Boolean,
+  available: Boolean,
+  documentation: Option[String]
+)
