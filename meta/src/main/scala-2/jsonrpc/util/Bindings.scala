50c582410d64c2fdcaad7e87b8994e50588c9d3b
Martin Ockajak <martin.ockajak@drion.ai>
2021-06-14 03:48:56 +0200

    Scala 2

---
+++ meta/src/main/scala-2/jsonrpc/util/Bindings.scala
@@ -0,0 +1,25 @@
+package jsonrpc.util
+
+import jsonrpc.protocol.MethodBindings.{methodSignature, validApiMethods}
+import scala.concurrent.Future
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox
+
+object Bindings {
+
+  def bind[T]: Unit = macro Bindings.bindMacro[T]
+
+  def bindMacro[T: c.WeakTypeTag](c: blackbox.Context): c.Expr[Unit] = {
+    import c.universe._
+
+    val ref = Reflection[c.type](c)
+    val apiMethods = validApiMethods[c.type, T, Future[_]](ref)
+    val validMethods = apiMethods.flatMap(_.toOption)
+    validMethods.foreach { method =>
+      println(methodSignature[c.type, T](ref)(method))
+    }
+    c.Expr[Unit](q"""
+      ()
+    """)
+  }
+}
