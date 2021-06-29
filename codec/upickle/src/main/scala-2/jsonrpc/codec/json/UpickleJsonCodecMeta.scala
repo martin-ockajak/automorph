79d9e6aec0b097dbf6a5c574b9f8f7694c17ab07
Martin Ockajak <martin.ockajak@drion.ai>
2021-06-29 17:01:57 +0200

    Cleanup

--- codec/upickle/src/main/scala-2/jsonrpc/codec/json/UpickleJsonCodecMeta.scala
+++ codec/upickle/src/main/scala-2/jsonrpc/codec/json/UpickleJsonCodecMeta.scala
@@ -2,6 +2,8 @@ package jsonrpc.codec.json
 
 import jsonrpc.codec.common.UpickleCustom
 import jsonrpc.spi.Codec
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox.Context
 import ujson.Value
 
 /**
@@ -12,10 +14,32 @@ import ujson.Value
 private[jsonrpc] trait UpickleJsonCodecMeta[Custom <: UpickleCustom] extends Codec[Value] {
   this: UpickleJsonCodec[Custom] =>
 
-  override def encode[T](value: T): Value = UpickleJsonCodecMacros.encode[Custom, T](custom, value)
-//  override def encode[T](value: T): Value = {
-//    custom.writeJs(value)
-//  }
+  override def encode[T](value: T): Value = macro UpickleJsonCodecMeta.encodeExpr[T]
 
-  override def decode[T](node: Value): T = UpickleJsonCodecMacros.decode[Custom, T](custom, node)
+  override def decode[T](node: Value): T = macro UpickleJsonCodecMeta.decodeExpr[T]
+}
+
+object UpickleJsonCodecMeta {
+
+  def encodeExpr[T: c.WeakTypeTag](c: Context)(value: c.Expr[T]): c.Expr[Value] = {
+    import c.universe.{weakTypeOf, Quasiquote}
+
+    val valueType = weakTypeOf[T]
+    c.Expr[Value](q"""
+      import custom._
+      val writer = implicitly[custom.Writer[$valueType]]
+      custom.writeJs($value)(writer)
+    """)
+  }
+
+  def decodeExpr[T: c.WeakTypeTag](c: Context)(node: c.Expr[Value]): c.Expr[T] = {
+    import c.universe.{weakTypeOf, Quasiquote}
+
+    val valueType = weakTypeOf[T]
+    c.Expr[T](q"""
+      import custom._
+      val reader = implicitly[custom.Reader[$valueType]]
+      custom.read[$valueType]($node)(reader)
+    """)
+  }
 }
--- codec/upickle/src/main/scala-2/jsonrpc/codec/messsagepack/UpickleMessagePackCodecMeta.scala
+++ codec/upickle/src/main/scala-2/jsonrpc/codec/messsagepack/UpickleMessagePackCodecMeta.scala
@@ -2,17 +2,44 @@ package jsonrpc.codec.messagepack
 
 import jsonrpc.codec.common.UpickleCustom
 import jsonrpc.spi.Codec
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox.Context
 import upack.Msg
 
 /**
- * UPickle JSON codec plugin code generation.
+ * UPickle MessagePack codec plugin code generation.
  *
  * @tparam Custom customized Upickle reader and writer implicits instance type
  */
 private[jsonrpc] trait UpickleMessagePackCodecMeta[Custom <: UpickleCustom] extends Codec[Msg] {
   this: UpickleMessagePackCodec[Custom] =>
 
-  override def encode[T](value: T): Msg = UpickleMessagePackCodecMacros.encode[Custom, T](custom, value)
+  override def encode[T](value: T): Msg = macro UpickleMessagePackCodecMeta.encodeExpr[T]
 
-  override def decode[T](node: Msg): T = UpickleMessagePackCodecMacros.decode[Custom, T](custom, node)
+  override def decode[T](node: Msg): T = macro UpickleMessagePackCodecMeta.decodeExpr[T]
+}
+
+object UpickleMessagePackCodecMeta {
+
+  def encodeExpr[T: c.WeakTypeTag](c: Context)(value: c.Expr[T]): c.Expr[Msg] = {
+    import c.universe.{weakTypeOf, Quasiquote}
+
+    val valueType = weakTypeOf[T]
+    c.Expr[Msg](q"""
+      import custom._
+      val writer = implicitly[custom.Writer[$valueType]]
+      custom.writeMsg($value)(writer)
+    """)
+  }
+
+  def decodeExpr[T: c.WeakTypeTag](c: Context)(node: c.Expr[Msg]): c.Expr[T] = {
+    import c.universe.{weakTypeOf, Quasiquote}
+
+    val valueType = weakTypeOf[T]
+    c.Expr[T](q"""
+      import custom._
+      val reader = implicitly[custom.Reader[$valueType]]
+      custom.readBinary[$valueType]($node)(reader)
+    """)
+  }
 }
