d865f4ebf50a05195eda5b72531527d9a91b6bfd
Martin Ockajak <martin.ockajak@drion.ai>
2021-06-30 02:13:37 +0200

    Cleanup

--- core/meta/src/main/scala-2/jsonrpc/handler/HandlerBindings.scala
+++ core/meta/src/main/scala-2/jsonrpc/handler/HandlerBindings.scala
@@ -142,6 +142,9 @@ case object HandlerBindings {
       // Create encode result function
       //   (result: ResultValueType) => Node = codec.encode[ResultValueType](result)
       val resultValueType = unwrapType[C, Effect[_]](ref)(method.resultType)
+      println(s"Result type: ${method.resultType}")
+      println(s"Effect type: ${weakTypeOf[Effect[_]]}")
+      println(s"Result value type: $resultValueType")
       val encodeResult = q"(result: $resultValueType) => $codec.encode[$resultValueType](result)"
 
       // Create the effect mapping call using the method call and the encode result function
