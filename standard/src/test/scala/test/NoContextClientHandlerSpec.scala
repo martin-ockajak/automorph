67e46d9615bac90f4a739af060d8b9fdbbd594c0
Martin Ockajak <martin.ockajak@drion.ai>
2021-06-30 17:40:54 +0200

    Cleanup

--- standard/src/test/scala/test/NoContextClientHandlerSpec.scala
+++ standard/src/test/scala/test/NoContextClientHandlerSpec.scala
@@ -19,6 +19,7 @@ class NoContextClientHandlerSpec extends BaseSpec {
       val handler = Handler.basic[Value, codec.type, Identity](codec, backend)
       val handlerTransport = HandlerTransport(handler, backend, Void.value)
       val client = Client.basic[Value, codec.type, Identity](codec, backend, handlerTransport)
+      client
 //      val result: String = client.callByPosition("test", 0)
 //      val result: String = client.callByName("test", "foo" -> 0)
     }
