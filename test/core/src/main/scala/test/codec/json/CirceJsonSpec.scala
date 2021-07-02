e65734095118f1abbbd342ef041080f863b82e40
Martin Ockajak <martin.ockajak@drion.ai>
2021-07-02 18:42:56 +0200

    Cleanup

--- test/core/src/main/scala/test/codec/json/CirceJsonSpec.scala
+++ test/core/src/main/scala/test/codec/json/CirceJsonSpec.scala
@@ -1,37 +1,35 @@
-//package test.codec.json
-//
-//import io.circe.*
-//import io.circe.generic.auto
-//import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
-//import io.circe.syntax.EncoderOps
-//import io.circe.{Decoder, Encoder, Json, parser}
-//import jsonrpc.Handler
-//import jsonrpc.codec.json.CirceJsonCodec
-//import jsonrpc.transport.local.HandlerTransport
-//import test.Enum.Enum
-//import test.{ClientHandlerSpec, ComplexApi, Handler, Record, Structure}
-//import test.codec.CodecSpec
-//import scala.language.implicitConversions
-//
-//trait CirceJsonSpec extends ClientHandlerSpec {
-//
-//  type Node = Json
-//  type CodecType = CirceJsonCodec[CirceJsonSpec.type]
-//
-//  override def codec: CodecType = CirceJsonCodec(CirceJsonSpec)
-//
-//  lazy val handler: Handler[Node, CodecType, Effect, Short] = Handler[Node, CodecType, Effect, Short](codec, backend)
-//    .bind(simpleApiInstance).bind[ComplexApi[Effect]](complexApiInstance)
-//
-//  lazy val handlerTransport: HandlerTransport[Node, CodecType, Effect, Short] = HandlerTransport(handler, backend, 0)
-//}
-//
-//object CirceJsonSpec extends CirceCustom {
-//
-//  given CirceEncoder[Enum] = Encoder.encodeInt.contramap[Enum](_.ordinal)
-//  given CirceDecoder[Enum] = Decoder.decodeInt.map(Enum.fromOrdinal)
-//  given CirceEncoder[Structure] = deriveEncoder[Structure]
-//  given CirceDecoder[Structure] = deriveDecoder[Structure]
-//  given CirceEncoder[Record] = deriveEncoder[Record]
-//  given CirceDecoder[Record] = deriveDecoder[Record]
-//}
+package test.codec.json
+
+import io.circe.generic.auto._
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.{Decoder, Encoder, Json}
+import jsonrpc.Handler
+import jsonrpc.codec.json.CirceJsonCodec
+import jsonrpc.transport.local.HandlerTransport
+import test.{ClientHandlerSpec, ComplexApi, Enum, InvalidApi, Record, SimpleApi, Structure}
+
+trait CirceJsonSpec extends ClientHandlerSpec {
+
+  type Node = Json
+  type CodecType = CirceJsonCodec
+
+  override def simpleApis: Seq[SimpleApi[Effect]] = clients.map(_.bind[SimpleApi[Effect]])
+
+  override def complexApis: Seq[ComplexApi[Effect, Context]] = clients.map(_.bind[ComplexApi[Effect, Context]])
+
+  override def invalidApis: Seq[InvalidApi[Effect]] = clients.map(_.bind[InvalidApi[Effect]])
+
+  def handler: Handler[Node, CodecType, Effect, Context]
+
+  implicit private lazy val enumEncoder: Encoder[Enum.Enum] = Encoder.encodeInt.contramap[Enum.Enum](Enum.toOrdinal)
+  implicit private lazy val enumDecoder: Decoder[Enum.Enum] = Decoder.decodeInt.map(Enum.fromOrdinal)
+  implicit private lazy val structureEncoder: Encoder[Structure] = deriveEncoder[Structure]
+  implicit private lazy val structureDecoder: Decoder[Structure] = deriveDecoder[Structure]
+
+  lazy val codec: CodecType = CirceJsonCodec()
+
+  lazy val handlerTransport: HandlerTransport[Node, CodecType, Effect, Context] = {
+    val boundHandler = handler.bind(simpleApiInstance).bind[ComplexApi[Effect, Context]](complexApiInstance)
+    HandlerTransport(boundHandler, backend, arbitraryContext.arbitrary.sample.get)
+  }
+}
