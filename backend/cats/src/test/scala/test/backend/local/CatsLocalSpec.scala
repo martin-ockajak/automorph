60b1cba0959db891895c307fac620b537320ae45
Martin Ockajak <martin.ockajak@drion.ai>
2021-07-03 18:19:57 +0200

    Backend Tests

--- backend/cats/src/test/scala/test/backend/local/CatsLocalSpec.scala
+++ backend/cats/src/test/scala/test/backend/local/CatsLocalSpec.scala
@@ -1,12 +1,11 @@
 package test.backend.local
 
-import jsonrpc.backend.CatsBackend
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
+import jsonrpc.backend.CatsBackend
 import jsonrpc.spi.Backend
 import jsonrpc.{Client, Handler}
 import org.scalacheck.Arbitrary
-import scala.util.Try
 import test.CodecClientHandlerSpec
 
 class CatsLocalSpec extends CodecClientHandlerSpec {
@@ -14,7 +13,7 @@ class CatsLocalSpec extends CodecClientHandlerSpec {
   type Effect[T] = IO[T]
   type Context = Short
 
-  override def run[T](effect: Effect[T]): T = Try(effect.unsafeRunSync()).get
+  override def run[T](effect: Effect[T]): T = effect.unsafeRunSync()
 
   override lazy val backend: Backend[Effect] = CatsBackend()
 
---
+++ backend/monix/src/test/scala/test/backend/local/MonixLocalSpec.scala
@@ -0,0 +1,22 @@
+package test.backend.local
+
+import jsonrpc.backend.MonixBackend
+import jsonrpc.spi.Backend
+import jsonrpc.{Client, Handler}
+import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
+import org.scalacheck.Arbitrary
+import scala.concurrent.duration.Duration
+import test.CodecClientHandlerSpec
+
+class MonixLocalSpec extends CodecClientHandlerSpec {
+
+  type Effect[T] = Task[T]
+  type Context = Short
+
+  override def run[T](effect: Effect[T]): T = effect.runSyncUnsafe(Duration.Inf)
+
+  override lazy val backend: Backend[Effect] = MonixBackend()
+
+  override lazy val arbitraryContext: Arbitrary[Context] = Arbitrary(Arbitrary.arbitrary[Context])
+}
