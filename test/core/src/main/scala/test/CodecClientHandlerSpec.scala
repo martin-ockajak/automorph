199954df1c90087355d0b799207c59896730e369
Martin Ockajak <martin.ockajak@drion.ai>
2021-07-03 04:40:21 +0200

    Tests

--- test/core/src/main/scala/test/CodecClientHandlerSpec.scala
+++ test/core/src/main/scala/test/CodecClientHandlerSpec.scala
@@ -5,12 +5,14 @@ import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 import io.circe.{Decoder, Encoder, Json}
 import jsonrpc.codec.common.UpickleCustom
 import jsonrpc.codec.json.{CirceJsonCodec, UpickleJsonCodec}
+import jsonrpc.codec.messagepack.UpickleMessagePackCodec
 import jsonrpc.spi.{Backend, Codec, Transport}
 import jsonrpc.transport.local.HandlerTransport
 import jsonrpc.{Client, Handler}
 import scala.util.Try
 import test.{ComplexApi, ComplexApiImpl, InvalidApi, InvalidApiImpl, SimpleApi, SimpleApiImpl}
 import ujson.Value
+import upack.Msg
 
 trait CodecClientHandlerSpec extends ClientHandlerSpec {
 
@@ -18,6 +20,37 @@ trait CodecClientHandlerSpec extends ClientHandlerSpec {
     {
       val codec = UpickleJsonCodec(CodecClientHandlerSpec)
       val handler = Handler[Value, UpickleJsonCodec[CodecClientHandlerSpec.type], Effect, Context](codec, backend)
+        .bind(simpleApiInstance).bind[ComplexApi[Effect, Context]](complexApiInstance)
+      val transport = HandlerTransport(handler, backend, arbitraryContext.arbitrary.sample.get)
+      val client = Client(codec, backend, transport)
+      val clients = makeClients(client)
+      CodecFixture(
+        codec.getClass,
+        client,
+        handler,
+        clients.map(_.bind[SimpleApi[Effect]]),
+        clients.map(_.bind[ComplexApi[Effect, Context]]),
+        clients.map(_.bind[InvalidApi[Effect]]),
+        (method, p1, context) => {
+          implicit val usingContext = context
+          client.callByPosition[String, String](method, p1)
+        },
+        (method, p1, context) => {
+          implicit val usingContext = context
+          client.callByName[String, String](method, p1)
+        },
+        (method, p1, context) => {
+          implicit val usingContext = context
+          client.notifyByPosition[String](method, p1)
+        },
+        (method, p1, context) => {
+          implicit val usingContext = context
+          client.notifyByName[String](method, p1)
+        }
+      )
+    }, {
+      val codec = UpickleMessagePackCodec(CodecClientHandlerSpec)
+      val handler = Handler[Msg, UpickleMessagePackCodec[CodecClientHandlerSpec.type], Effect, Context](codec, backend)
         .bind(simpleApiInstance).bind[ComplexApi[Effect, Context]](complexApiInstance)
       val transport = HandlerTransport(handler, backend, arbitraryContext.arbitrary.sample.get)
       val client = Client(codec, backend, transport)
