7efe23a60fe483d88bb525fd4ec5570b0b55d087
Martin Ockajak <martin.ockajak@drion.ai>
2021-07-06 13:38:05 +0200

    Rename script

--- tools/rename.sh
+++ tools/rename.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for f in `find . -type f -name '*.scala' -o -name '*.java' -o -name '*.sbt' -o -name '*.xml' | grep src`; do echo sed -i 's/jsonrpc/automorph/g' "$f" ; done
-for f in `find . -type d -name jsonrpc | grep src`; do echo git mv "$f" "`echo $f | sed 's/jsonrpc/automorph/g'`" ; done
+for f in `find . -type f -name '*.scala' -o -name '*.java' -o -name '*.sbt' -o -name '*.xml' | grep src`; do sed -i 's/jsonrpc/automorph/g' "$f" ; done
+for f in `find . -type d -name jsonrpc | grep src`; do git mv "$f" "`echo $f | sed 's/jsonrpc/automorph/g'`" ; done
 
