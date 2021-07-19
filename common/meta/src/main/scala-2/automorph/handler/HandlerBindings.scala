019463f5431b58ef25141a25b462120ec0cde324
Martin Ockajak <m@polytree.org>
2021-07-19 17:45:28 +0200

    Bind

--- common/meta/src/main/scala-2/automorph/handler/HandlerBindings.scala
+++ common/meta/src/main/scala-2/automorph/handler/HandlerBindings.scala
@@ -35,10 +35,114 @@ case object HandlerBindings {
 
     // Generate bound API method bindings
     val handlerBindings = validMethods.map { method =>
-      q"${method.name} -> ${generateBinding[c.type, Node, ActualFormat, Effect, Context, Api](ref)(method, format, system, api)}"
+      q"${method.name} -> ${brokenGenerateBinding[c.type, Node, ActualFormat, Effect, Context, Api](ref)(method, format, system, api)}"
     }
     c.Expr[Map[String, HandlerBinding[Node, Effect, Context]]](q"""
       Seq(..$handlerBindings).toMap
+    """)
+  }
+
+  private def brokenGenerateBinding[
+    C <: blackbox.Context,
+    Node: ref.c.WeakTypeTag,
+    ActualFormat <: MessageFormat[Node]: ref.c.WeakTypeTag,
+    Effect[_],
+    Context: ref.c.WeakTypeTag,
+    Api: ref.c.WeakTypeTag
+  ](ref: Reflection[C])(
+    method: ref.RefMethod,
+    format: ref.c.Expr[ActualFormat],
+    system: ref.c.Expr[EffectSystem[Effect]],
+    api: ref.c.Expr[Api]
+  )(implicit effectType: ref.c.WeakTypeTag[Effect[_]]): ref.c.Expr[HandlerBinding[Node, Effect, Context]] = {
+    import ref.c.universe.{Liftable, Quasiquote}
+
+    val invoke = brokenGenerateInvoke[C, Node, ActualFormat, Effect, Context, Api](ref)(method, format, system, api)
+    logBoundMethod[C, Api](ref)(method, invoke)
+    implicit val methodLift: Liftable[Method] = methodLiftable(ref)
+    Seq(methodLift)
+    ref.c.Expr[HandlerBinding[Node, Effect, Context]](q"""
+      automorph.handler.HandlerBinding(
+        ${method.lift},
+        $invoke,
+        ${methodUsesContext[C, Context](ref)(method)}
+      )
+    """)
+  }
+
+  private def brokenGenerateInvoke[
+    C <: blackbox.Context,
+    Node: ref.c.WeakTypeTag,
+    ActualFormat <: MessageFormat[Node]: ref.c.WeakTypeTag,
+    Effect[_],
+    Context: ref.c.WeakTypeTag,
+    Api
+  ](ref: Reflection[C])(
+    method: ref.RefMethod,
+    format: ref.c.Expr[ActualFormat],
+    system: ref.c.Expr[EffectSystem[Effect]],
+    api: ref.c.Expr[Api]
+  )(implicit effectType: ref.c.WeakTypeTag[Effect[_]]): ref.c.Expr[(Seq[Node], Context) => Effect[Node]] = {
+    import ref.c.universe.{weakTypeOf, Quasiquote}
+    (weakTypeOf[Node], weakTypeOf[ActualFormat])
+
+    // Map multiple parameter lists to flat argument node list offsets
+    val parameterListOffsets = method.parameters.map(_.size).foldLeft(Seq(0)) { (indices, size) =>
+      indices :+ (indices.last + size)
+    }
+    val lastArgumentIndex = method.parameters.map(_.size).sum - 1
+
+    // Create invoke function
+    //   (argumentNodes: Seq[Node], context: Context) => Effect[Node]
+    val nodeType = weakTypeOf[Node].dealias
+    val contextType = weakTypeOf[Context].dealias
+    ref.c.Expr[(Seq[Node], Context) => Effect[Node]](q"""
+      (argumentNodes: Seq[$nodeType], context: $contextType) => ${
+      // Create the method argument lists by decoding corresponding argument nodes into values
+      //   List(List(
+      //     (Try(format.decode[Parameter0Type](argumentNodes(0))) match {
+      //       case Failure(error) => Failure(InvalidRequestException("Invalid argument: " + ${ Expr(argumentIndex) }, error))
+      //       case result => result
+      //     }).get
+      //     ...
+      //     (Try(format.decode[ParameterNType](argumentNodes(N))) match {
+      //       case Failure(error) => Failure(InvalidRequestException("Invalid argument: " + ${ Expr(argumentIndex) }, error))
+      //       case result => result
+      //     }).get
+      //   )): List[List[ParameterXType]]
+      val arguments = method.parameters.toList.zip(parameterListOffsets).map { case (parameters, offset) =>
+        parameters.toList.zipWithIndex.map { case (parameter, index) =>
+          val argumentIndex = offset + index
+          if (argumentIndex == lastArgumentIndex && methodUsesContext[C, Context](ref)(method)) {
+            q"context"
+          } else {
+            q"""
+              (scala.util.Try($format.decode[${parameter.dataType}](argumentNodes($argumentIndex))) match {
+                case scala.util.Failure(error) => scala.util.Failure(
+                  automorph.protocol.ErrorType.InvalidRequestException("Invalid argument: " + $argumentIndex, error)
+                )
+                case result => result
+              }).get
+             """
+          }
+        }
+      }
+
+      // Create the API method call using the decoded arguments
+      //   api.method(arguments ...): Effect[ResultValueType]
+      val apiMethodCall = q"$api.${method.symbol}(...$arguments)"
+
+      // Create encode result function
+      //   (result: ResultValueType) => Node = format.encode[ResultValueType](result)
+      val resultValueType = unwrapType[C, Effect[_]](ref.c)(method.resultType).dealias
+      val encodeResult = q"(result: $resultValueType) => $format.encode[$resultValueType](result)"
+
+      // Create the effect mapping call using the method call and the encode result function
+      //   system.map(methodCall, encodeResult): Effect[Node]
+      q"$system.map($apiMethodCall, $encodeResult)"
+      //      arguments.flatten.foreach(println(ref.c.universe.showCode(_)))
+      //      q"null.asInstanceOf[$effectType[$resultValueType]]"
+    }
     """)
   }
 
