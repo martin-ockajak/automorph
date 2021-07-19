79c121b04a16d35b6e3839796d60d43608d26aed
Martin Ockajak <m@polytree.org>
2021-07-19 17:10:57 +0200

    Bind

--- test/examples/src/test/scala/test/examples/BrokenBind.scala
+++ test/examples/src/test/scala/test/examples/BrokenBind.scala
@@ -19,3 +19,19 @@ trait BrokenBind[Effect[_], Context] {
     .bind(simpleApiInstance)
     .bind(complexApiInstance)
 }
+
+trait SimpleApi[Effect[_]] {
+
+  def test(test: String): Effect[String]
+}
+
+final case class SimpleApiImpl[Effect[_]](backend: EffectSystem[Effect]) extends SimpleApi[Effect] {
+
+  override def test(test: String): Effect[String] = backend.pure(test)
+}
+
+trait ComplexApi[Effect[_], Context] {
+}
+
+final case class ComplexApiImpl[Effect[_], Context](backend: EffectSystem[Effect]) extends ComplexApi[Effect, Context] {
+}
