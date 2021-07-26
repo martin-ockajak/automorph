dd22b8b7ca6b7523add76926340a4d3de8ea42ae
Martin Ockajak <m@polytree.org>
2021-07-26 19:11:07 +0200

    OpenAPI

--- common/openapi/src/main/scala/automorph/openapi/Generator.scala
+++ common/openapi/src/main/scala/automorph/openapi/Generator.scala
@@ -8,14 +8,19 @@ import automorph.util.Method
  * @see https://github.com/OAI/OpenAPI-Specification
  */
 case object Generator {
+
   /**
    * Generate OpenAPI specification for given API methods.
    *
    * @param methods named API methods
    * @param info OpenAPI Info object
+   * @param serverUrls server URLs
    * @return OpenAPI specification
    */
-  def jsonRpc(methods: Map[String, Method], info: Info): Specification = ???
+  def jsonRpc(methods: Map[String, Method], info: Info, serverUrls: Seq[String] = Seq()): Specification = {
+    val servers = createServers(serverUrls)
+    Specification(info = info, servers = servers)
+  }
 
   /**
    * Generate OpenAPI specification for given API methods.
@@ -25,17 +30,24 @@ case object Generator {
    * @param version API specification version
    * @return OpenAPI specification
    */
-  def jsonRpc(methods: Map[String, Method], title: String, version: String): Specification =
-    jsonRpc(methods, Info(title = title, version = version))
+  def jsonRpc(
+    methods: Map[String, Method],
+    title: String,
+    version: String
+  ): Specification = jsonRpc(methods, Info(title = title, version = version))
 
   /**
    * Generate OpenAPI specification for given API methods.
    *
    * @param methods named API methods
    * @param info OpenAPI Info object
+   * @param serverUrls server URLs
    * @return OpenAPI specification
    */
-  def restRpc(methods: Map[String, Method], info: Info): Specification = ???
+  def restRpc(methods: Map[String, Method], info: Info, serverUrls: Seq[String] = Seq()): Specification = {
+    val servers = createServers(serverUrls)
+    Specification(info = info, servers = servers)
+  }
 
   /**
    * Generate OpenAPI specification for given API methods.
@@ -45,6 +57,14 @@ case object Generator {
    * @param version API specification version
    * @return OpenAPI specification
    */
-  def restRpc(methods: Map[String, Method], title: String, version: String): Specification =
-    restRpc(methods, Info(title = title, version = version))
+  def restRpc(
+    methods: Map[String, Method],
+    title: String,
+    version: String
+  ): Specification = restRpc(methods, Info(title = title, version = version))
+
+  private def createServers(serverUrls: Seq[String]): Option[List[Server]] = serverUrls match {
+    case Seq() => None
+    case Seq(urls: _*) => Some(urls.map(url => Server(url = url)).toList.toList)
+  }
 }
