6e502d31d52fab3a2be9be7efffdf08c24035e0d
Martin Ockajak <m@polytree.org>
2021-07-26 19:55:03 +0200

    OpenAPI

--- common/openapi/src/main/scala/automorph/openapi/Generator.scala
+++ common/openapi/src/main/scala/automorph/openapi/Generator.scala
@@ -11,18 +11,12 @@ import automorph.util.Method
 case object Generator {
 
   /**
-   * Generate OpenAPI specification for given API methods.
+   * Generate OpenAPI paths for given API methods.
    *
-   * @param methods named API methods
-   * @param info OpenAPI Info object
-   * @param servers OpenAPI Server objects
-   * @return OpenAPI specification
+   * @param methods named API
+   * @return OpenAPI paths
    */
-  def jsonRpc(methods: Map[String, Method], info: Info, servers: Seq[Server]): Specification = {
-    val paths = toPaths(methods, true)
-    val components = toComponents()
-    Specification(info = info, servers = toServers(servers), paths = paths, components = components)
-  }
+  def jsonRpcPaths(methods: Map[String, Method]): Paths = toPaths(methods, true)
 
   /**
    * Generate OpenAPI specification for given API methods.
@@ -33,22 +27,25 @@ case object Generator {
    * @param serverUrls API server URL
    * @return OpenAPI specification
    */
-  def jsonRpc(methods: Map[String, Method], title: String, version: String, serverUrls: Seq[String]): Specification =
-    jsonRpc(methods, Info(title = title, version = version), toServers(serverUrls))
+  def jsonRpcSpec(
+    methods: Map[String, Method],
+    title: String,
+    version: String,
+    serverUrls: Seq[String]
+  ): Specification =
+    Specification(
+      paths = Some(toPaths(methods, true)),
+      info = Info(title = title, version = version),
+      servers = toServers(serverUrls)
+    )
 
   /**
-   * Generate OpenAPI specification for given API methods.
+   * Generate OpenAPI paths for given API methods.
    *
    * @param methods named API methods
-   * @param info OpenAPI Info object
-   * @param servers OpenAPI Server objects
-   * @return OpenAPI specification
+   * @return OpenAPI paths
    */
-  def restRpc(methods: Map[String, Method], info: Info, servers: Seq[Server]): Specification = {
-    val paths = toPaths(methods, false)
-    val components = toComponents()
-    Specification(info = info, servers = toServers(servers), paths = paths, components = components)
-  }
+  def restRpcPaths(methods: Map[String, Method]): Paths = toPaths(methods, false)
 
   /**
    * Generate OpenAPI specification for given API methods.
@@ -59,24 +56,27 @@ case object Generator {
    * @param serverUrls API server URL
    * @return OpenAPI specification
    */
-  def restRpc(methods: Map[String, Method], title: String, version: String, serverUrls: Seq[String]): Specification =
-    restRpc(methods, Info(title = title, version = version), toServers(serverUrls))
+  def restRpcSpec(
+    methods: Map[String, Method],
+    title: String,
+    version: String,
+    serverUrls: Seq[String]
+  ): Specification =
+    Specification(
+      paths = Some(toPaths(methods, false)),
+      info = Info(title = title, version = version),
+      servers = toServers(serverUrls)
+    )
 
-  private def toServers(serverUrls: Seq[String]): Seq[Server] = serverUrls.map(url => Server(url = url))
-
-  private def toServers(servers: Seq[Server]): Option[Servers] = servers match {
+  private def toServers(serverUrls: Seq[String]): Option[Servers] = serverUrls match {
     case Seq() => None
-    case someServers => Some(someServers.toList)
+    case someServers => Some(serverUrls.map(url => Server(url = url)).toList)
   }
 
-  private def toPaths(methods: Map[String, Method], rpc: Boolean): Option[Paths] = methods match {
-    case noMethods if noMethods.isEmpty => None
-    case someMethods =>
-      Some(someMethods.map { case (name, method) =>
-        val path = s"/${name.replace('.', '/')}"
-        val pathItem = if (rpc) jsonRpcPathItem(method) else restRpcPathItem(method)
-        path -> pathItem
-      }.toMap)
+  private def toPaths(methods: Map[String, Method], rpc: Boolean): Paths = methods.map { case (name, method) =>
+    val path = s"/${name.replace('.', '/')}"
+    val pathItem = if (rpc) jsonRpcPathItem(method) else restRpcPathItem(method)
+    path -> pathItem
   }
 
   private def jsonRpcPathItem(method: Method): PathItem =
