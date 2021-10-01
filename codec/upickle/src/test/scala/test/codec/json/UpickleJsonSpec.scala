646eb526311eacede173aaa5017ba44a22c7bc3d
Martin Ockajak <m@polytree.org>
2021-10-02 00:39:40 +0200

    Cleanup

--- codec/upickle/src/test/scala/test/codec/json/UpickleJsonSpec.scala
+++ codec/upickle/src/test/scala/test/codec/json/UpickleJsonSpec.scala
@@ -1,52 +1,49 @@
-// FIXME - enable
-//package test.codec.json
-//
-//import automorph.codec.json.UpickleJsonCodec
-//import automorph.codec.UpickleCustom
-//import org.scalacheck.{Arbitrary, Gen}
-//import test.Generators.arbitraryRecord
-////import test.codec.JsonMessageCodecSpec
-//import test.codec.MessageCodecSpec
-//import test.{Enum, Record, Structure}
-//import ujson.{Arr, Bool, Num, Obj, Str, Value}
-//
-////class UpickleJsonSpec extends JsonMessageCodecSpec {
-//class UpickleJsonSpec extends MessageCodecSpec {
-//
-//  type Node = Value
-//  type ActualCodec = UpickleJsonCodec[UpickleJsonSpec.type]
-//
-//  override def codec: ActualCodec = UpickleJsonCodec(UpickleJsonSpec)
-//
-//  override lazy val arbitraryNode: Arbitrary[Node] = Arbitrary(Gen.recursive[Node](recurse =>
-//    Gen.oneOf(
-//      Gen.resultOf(Str(_)),
-//      Gen.resultOf(Num(_)),
-//      Gen.resultOf(Bool(_)),
-//      Gen.listOfN[Node](2, recurse).map(Arr(_: _*)),
-//      Gen.mapOfN(2, Gen.zip(Arbitrary.arbitrary[String], recurse)).map(Obj.from)
-//    )
-//  ))
-//
-//  private lazy val custom = codec.custom
-//  implicit private lazy val recordRw: custom.ReadWriter[Record] = custom.macroRW
-//
-//  "" - {
-//    "Encode / Decode" in {
-//      check { (record: Record) =>
-//        val encodedValue = codec.encode(record)
-//        val decodedValue = codec.decode[Record](encodedValue)
-//        decodedValue.equals(record)
-//      }
-//    }
-//  }
-//}
-//
-//object UpickleJsonSpec extends UpickleCustom {
-//
-//  implicit lazy val enumRw: ReadWriter[Enum.Enum] = readwriter[Int].bimap[Enum.Enum](
-//    value => Enum.toOrdinal(value),
-//    number => Enum.fromOrdinal(number)
-//  )
-//  implicit lazy val structureRw: ReadWriter[Structure] = macroRW
-//}
+package test.codec.json
+
+import automorph.codec.UpickleCustom
+import automorph.codec.json.UpickleJsonCodec
+import org.scalacheck.{Arbitrary, Gen}
+import test.Generators.arbitraryRecord
+import test.codec.MessageCodecSpec
+import test.{Enum, Record, Structure}
+import ujson.{Arr, Bool, Num, Obj, Str, Value}
+
+class UpickleJsonSpec extends MessageCodecSpec {
+
+  type Node = Value
+  type ActualCodec = UpickleJsonCodec[UpickleJsonSpec.type]
+
+  override def codec: ActualCodec = UpickleJsonCodec(UpickleJsonSpec)
+
+  override lazy val arbitraryNode: Arbitrary[Node] = Arbitrary(Gen.recursive[Node](recurse =>
+    Gen.oneOf(
+      Gen.resultOf(Str(_)),
+      Gen.resultOf(Num(_)),
+      Gen.resultOf(Bool(_)),
+      Gen.listOfN[Node](2, recurse).map(Arr(_: _*)),
+      Gen.mapOfN(2, Gen.zip(Arbitrary.arbitrary[String], recurse)).map(Obj.from)
+    )
+  ))
+
+  private lazy val custom = codec.custom
+  implicit private lazy val recordRw: custom.ReadWriter[Record] = custom.macroRW
+
+  "" - {
+    "Encode / Decode" in {
+      check { (record: Record) =>
+        val encodedValue = codec.encode(record)
+        val decodedValue = codec.decode[Record](encodedValue)
+        decodedValue.equals(record)
+      }
+    }
+  }
+}
+
+object UpickleJsonSpec extends UpickleCustom {
+
+  implicit lazy val enumRw: ReadWriter[Enum.Enum] = readwriter[Int].bimap[Enum.Enum](
+    value => Enum.toOrdinal(value),
+    number => Enum.fromOrdinal(number)
+  )
+  implicit lazy val structureRw: ReadWriter[Structure] = macroRW
+}
--- test/plugin/src/main/scala/test/codec/MessageCodecSpec.scala
+++ test/plugin/src/main/scala/test/codec/MessageCodecSpec.scala
@@ -1,15 +1,18 @@
 package test.codec
 
-import java.nio.charset.StandardCharsets
 import automorph.spi.MessageCodec
+import java.nio.charset.StandardCharsets
 import org.scalacheck.Arbitrary
-import test.Generators
+import test.{Enum, Generators, Record, Structure}
 import test.base.BaseSpec
 
 /**
  * Message codec test.
  *
  * Checks message serialization, deserialization and textual representation.
+ *
+ * @tparam Node message node type
+ * @tparam ActualCodec message codec
  */
 trait MessageCodecSpec extends BaseSpec {
 
@@ -20,7 +23,23 @@ trait MessageCodecSpec extends BaseSpec {
 
   implicit def arbitraryNode: Arbitrary[Node]
 
-  val charset = StandardCharsets.UTF_8
+  private val charset = StandardCharsets.UTF_8
+
+//  private val record = Record(
+//    "test",
+//    true,
+//    0,
+//    1,
+//    Some(2),
+//    3,
+//    4.5,
+//    6.7,
+//    Enum.Enum.One,
+//    List("foo", "bar"),
+//    Map("a" -> 0, "b" -> 1),
+//    Some(Structure("")),
+//    None
+//  )
 
   "" - {
     "Serialize / Deserialize" in {
