47562bbcf081988e5810f7643a1e080481eb56b9
Martin Ockajak <m@polytree.org>
2021-10-11 19:24:57 +0200

    Refactoring

--- test/examples/src/test/scala/test/examples/MessageCodec.scala
+++ test/examples/src/test/scala/test/examples/MessageCodec.scala
@@ -1,6 +1,6 @@
 package test.examples
 
-import automorph.codec.messagepack.UpickleMessagePackCodec
+import automorph.codec.messagepack.{UpickleMessagePackCodec, UpickleMessagePackCustom}
 import automorph.{Client, DefaultEffectSystem, DefaultHttpClientTransport, DefaultHttpServer, DefaultRpcProtocol, Handler}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -23,8 +23,8 @@ object MessageCodec extends App {
   val system = DefaultEffectSystem.async
 
   // Start RPC server listening on port 80 for HTTP requests with URL path '/api'
-  val protocol = DefaultRpcProtocol[UpickleMessagePackCodec.Node, codec.type](codec)
-  val handler = Handler[UpickleMessagePackCodec.Node, codec.type, Future, DefaultHttpServer.Context](
+  val protocol = DefaultRpcProtocol[UpickleMessagePackCodec.Node, UpickleMessagePackCodec[UpickleMessagePackCustom]](codec)
+  val handler = Handler[UpickleMessagePackCodec.Node, UpickleMessagePackCodec[UpickleMessagePackCustom], Future, DefaultHttpServer.Context](
     system,
     protocol
   )
@@ -33,7 +33,7 @@ object MessageCodec extends App {
   // Create RPC client for sending HTTP POST requests to 'http://localhost/api'
   val url = new java.net.URI("http://localhost/api")
   val transport = DefaultHttpClientTransport.async(url, "POST")
-  val client = Client[UpickleMessagePackCodec.Node, codec.type, Future, DefaultHttpClientTransport.Context](
+  val client = Client[UpickleMessagePackCodec.Node, UpickleMessagePackCodec[UpickleMessagePackCustom], Future, DefaultHttpClientTransport.Context](
     system,
     protocol,
     transport
