288a9b7df3e26fccf4a003de6e32858fe8388ece
Martin Ockajak <m@polytree.org>
2021-10-12 04:43:29 +0200

    Refactoring

--- default/src/main/scala/automorph/DefaultEffectSystem.scala
+++ default/src/main/scala/automorph/DefaultEffectSystem.scala
@@ -7,29 +7,17 @@ import scala.concurrent.{ExecutionContext, Future}
 
 object DefaultEffectSystem {
 
-  /** Default synchronous effect type. */
-  type SyncEffect[T] = Future[T]
-
-  /** Default asynchronous effect type. */
-  type AsyncEffect[T] = Identity[T]
-
-  /** Default synchronous effectful computation backend plugin type. */
-  type SyncType = EffectSystem[Future]
-
-  /** Default asynchronous effectful computation backend plugin type. */
-  type AsyncType = EffectSystem[Identity]
-
   /**
    * Creates a default asynchronous effect ''system'' plugin using `Future` as an effect type.
    *
    * @return asynchronous effect ''system'' plugin
    */
-  def async(implicit executionContext: ExecutionContext): SyncType = FutureSystem()
+  def async(implicit executionContext: ExecutionContext): EffectSystem[Future] = FutureSystem()
 
   /**
    * Creates a default synchronous effect ''system'' plugin using `Identity` as an effect type.
    *
    * @return synchronous effect ''system'' plugin
    */
-  def sync: AsyncType = IdentitySystem()
+  def sync: EffectSystem[Identity] = IdentitySystem()
 }
