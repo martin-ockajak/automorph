94ebbb5f0b5868ba99df9d29ab05c14bb0f22b8f
Martin Ockajak <m@polytree.org>
2021-10-17 17:37:33 +0200

    Refactoring

--- system/standard/src/test/scala-3/test/EmptyContextSpec.scala
+++ system/standard/src/test/scala-3/test/EmptyContextSpec.scala
@@ -6,20 +6,20 @@ import automorph.protocol.jsonrpc.Message
 import automorph.system.IdentitySystem
 import automorph.system.IdentitySystem.Identity
 import automorph.transport.local.client.HandlerTransport
-import automorph.util.Context
+import automorph.util.EmptyContext
 import automorph.{Client, Handler}
 import io.circe.{Decoder, Encoder, Json}
 import test.base.BaseSpec
 
-class ContextSpec extends BaseSpec {
+class EmptyContextSpec extends BaseSpec {
 
   "" - {
     "Create" in {
       val codec = CirceJsonCodec()
       val protocol = JsonRpcProtocol(codec)
       val system = IdentitySystem()
-      val handler = Handler.protocol(protocol).system(system).context[Context.Value]
-      val transport = HandlerTransport(handler, system, Context.value)
+      val handler = Handler.protocol(protocol).system(system).context[EmptyContext.Value]
+      val transport = HandlerTransport(handler, system, EmptyContext.value)
       val client = Client.protocol(protocol).transport(transport)
       client
     }
