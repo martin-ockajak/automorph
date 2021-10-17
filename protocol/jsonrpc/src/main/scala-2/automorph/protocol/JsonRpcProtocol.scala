b0f98f67848b53dd501ce8dcf99c71eaaebc25d2
Martin Ockajak <m@polytree.org>
2021-10-18 00:48:29 +0200

    Refactoring

--- protocol/jsonrpc/src/main/scala-2/automorph/protocol/JsonRpcProtocol.scala
+++ protocol/jsonrpc/src/main/scala-2/automorph/protocol/JsonRpcProtocol.scala
@@ -48,10 +48,17 @@ object JsonRpcProtocol extends ErrorMapping {
     errorToException: (String, Int) => Throwable = defaultErrorToException,
     exceptionToError: Throwable => ErrorType = defaultExceptionToError,
     argumentsByName: Boolean = true
-  ): JsonRpcProtocol[Node, Codec] =
-    macro applyMacro[Node, Codec]
+  ): JsonRpcProtocol[Node, Codec] = JsonRpcProtocol(codec, errorToException, exceptionToError, argumentsByName)
 
-  def applyMacro[Node: c.WeakTypeTag, Codec <: MessageCodec[Node]: c.WeakTypeTag](c: blackbox.Context)(
+  private def create[Node, Codec <: MessageCodec[Node]](
+    codec: Codec,
+    errorToException: (String, Int) => Throwable,
+    exceptionToError: Throwable => ErrorType,
+    argumentsByName: Boolean
+  ): JsonRpcProtocol[Node, Codec] =
+    macro createMacro[Node, Codec]
+
+  def createMacro[Node: c.WeakTypeTag, Codec <: MessageCodec[Node]: c.WeakTypeTag](c: blackbox.Context)(
     codec: c.Expr[Codec],
     errorToException: c.Expr[(String, Int) => Throwable],
     exceptionToError: c.Expr[Throwable => ErrorType],
