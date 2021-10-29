18c422d4b249232874c6987c8dfab6f9ec8fc712
Martin Ockajak <m@polytree.org>
2021-10-29 22:52:23 +0200

    Refactoring

--- system/standard/src/test/scala-2/test/EmptyContextSpec.scala
+++ system/standard/src/test/scala-2/test/EmptyContextSpec.scala
@@ -1,12 +1,12 @@
 package test
 
-import automorph.system.IdentitySystem
-import automorph.system.IdentitySystem.Identity
 import automorph.codec.json.CirceJsonCodec
+import automorph.protocol.JsonRpcProtocol
+import automorph.system.IdentitySystem
 import automorph.transport.local.client.HandlerTransport
 import automorph.util.EmptyContext
 import automorph.{Client, Handler}
-import ujson.Value
+import test.base.BaseSpec
 
 class EmptyContextSpec extends BaseSpec {
 
@@ -15,7 +15,7 @@ class EmptyContextSpec extends BaseSpec {
       val codec = CirceJsonCodec()
       val protocol = JsonRpcProtocol(codec)
       val system = IdentitySystem()
-      val handler = Handler.protocol(protocol).system(system).context[Context.Value]
+      val handler = Handler.protocol(protocol).system(system).emptyContext
       val transport = HandlerTransport(handler, system, EmptyContext.value)
       val client = Client.protocol(protocol).transport(transport)
       client
--- system/standard/src/test/scala-3/test/EmptyContextSpec.scala
+++ system/standard/src/test/scala-3/test/EmptyContextSpec.scala
@@ -2,13 +2,10 @@ package test
 
 import automorph.codec.json.CirceJsonCodec
 import automorph.protocol.JsonRpcProtocol
-import automorph.protocol.jsonrpc.Message
 import automorph.system.IdentitySystem
-import automorph.system.IdentitySystem.Identity
 import automorph.transport.local.client.HandlerTransport
 import automorph.util.EmptyContext
 import automorph.{Client, Handler}
-import io.circe.{Decoder, Encoder, Json}
 import test.base.BaseSpec
 
 class EmptyContextSpec extends BaseSpec {
@@ -18,7 +15,7 @@ class EmptyContextSpec extends BaseSpec {
       val codec = CirceJsonCodec()
       val protocol = JsonRpcProtocol(codec)
       val system = IdentitySystem()
-      val handler = Handler.protocol(protocol).system(system).context[EmptyContext.Value]
+      val handler = Handler.protocol(protocol).system(system).emptyContext
       val transport = HandlerTransport(handler, system, EmptyContext.value)
       val client = Client.protocol(protocol).transport(transport)
       client
