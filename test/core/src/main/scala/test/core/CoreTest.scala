e0c51c3451229829ea1d81f966c6d981bb18062c
Martin Ockajak <m@polytree.org>
2021-10-30 01:30:06 +0200

    Refactoring

--- test/core/src/main/scala/test/core/CoreTest.scala
+++ test/core/src/main/scala/test/core/CoreTest.scala
@@ -51,7 +51,7 @@ trait CoreTest extends BaseTest {
   "" - {
 // FIXME - restore
 //    fixtures.foreach { fixture =>
-    fixtures.headOption.foreach { fixture =>
+    fixtures.take(1).foreach { fixture =>
       val codecName = fixture.genericClient.protocol.codec.getClass.getSimpleName.replaceAll("MessageCodec$", "")
       codecName - {
         "Static" - {
