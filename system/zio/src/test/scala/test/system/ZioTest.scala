8153ac687d0fbf81a195ce898c386b333f5b1070
Martin Ockajak <m@polytree.org>
2021-10-30 16:21:31 +0200

    Refactoring

--- system/zio/src/test/scala/test/system/ZioTest.scala
+++ system/zio/src/test/scala/test/system/ZioTest.scala
@@ -1,14 +1,23 @@
 package test.system
 
+import ZioTest.ZioEffect
 import automorph.system.ZioSystem
 import automorph.spi.EffectSystem
+import automorph.spi.system.Defer
 import scala.util.Try
 import zio.{RIO, Runtime, ZEnv}
 
-class ZioTest extends EffectSystemTest[({ type Effect[T] = RIO[ZEnv, T] })#Effect] {
+class ZioTest extends DeferEffectSystemTest[ZioEffect] {
+
   private lazy val runtime = Runtime.default.withReportFailure(_ => ())
 
-  def system: EffectSystem[({ type Effect[T] = RIO[ZEnv, T] })#Effect] = ZioSystem[ZEnv]()
+  def deferSystem: EffectSystem[ZioEffect] with Defer[ZioEffect] =
+    ZioSystem[ZEnv]()
 
   def run[T](effect: RIO[ZEnv, T]): Either[Throwable, T] = Try(runtime.unsafeRunTask(effect)).toEither
+}
+
+object ZioTest {
+
+  type ZioEffect[T] = RIO[ZEnv, T]
 }
