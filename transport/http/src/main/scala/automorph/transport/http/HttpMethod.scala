3e15b6505e96c5dcad23cd8bb18aa05d8eef1579
Martin Ockajak <m@polytree.org>
2021-11-03 00:50:02 +0100

    Refactoring

---
+++ transport/http/src/main/scala/automorph/transport/http/HttpMethod.scala
@@ -0,0 +1,36 @@
+package automorph.transport.http
+
+/** HTTP request method. */
+sealed abstract class HttpMethod {
+  /** HTTP method name. */
+  def name: String = toString.toUpperCase
+}
+
+object HttpMethod {
+
+  /**
+   * Create HTTP method with specified name.
+   *
+   * @name HTTP method name
+   * @return HTTP method
+   */
+  def valueOf(name: String): HttpMethod = {
+    values.find(_.name == name.toUpperCase).getOrElse {
+      throw new IllegalArgumentException(s"Invalid HTTP method: $name")
+    }
+  }
+
+  /** HTTP method values. */
+  lazy val values: Seq[HttpMethod] =
+    Seq(Get, Head, Post, Put, Delete, Connect, Options, Trace, Patch)
+
+  case object Get extends HttpMethod
+  case object Head extends HttpMethod
+  case object Post extends HttpMethod
+  case object Put extends HttpMethod
+  case object Delete extends HttpMethod
+  case object Connect extends HttpMethod
+  case object Options extends HttpMethod
+  case object Trace extends HttpMethod
+  case object Patch extends HttpMethod
+}
