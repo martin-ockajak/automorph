c0e54c006a2dd434e50df6254d4a1873001c2046
Martin Ockajak <m@polytree.org>
2021-11-03 15:09:38 +0100

    Undertow

--- transport/undertow/src/test/scala/test/transport/websocket/UndertowWebSocketFutureTest.scala
+++ transport/undertow/src/test/scala/test/transport/websocket/UndertowWebSocketFutureTest.scala
@@ -1,32 +1,32 @@
-//package test.transport.websocket
-//
-//import automorph.Types
-//import automorph.spi.transport.ServerMessageTransport
-//import automorph.system.FutureSystem
-//import automorph.transport.http.server.UndertowServer
-//import org.scalacheck.Arbitrary
-//import scala.collection.mutable.ArrayBuffer
-//import scala.concurrent.Future
-//import test.standard.StandardHttpServerTest
-//import test.transport.http.HttpContextGenerator
-//
-//class UndertowWebSocketFutureTest extends StandardHttpServerTest {
-//
-//  type Effect[T] = Future[T]
-//  type Context = UndertowServer.Context
-//
-//  override lazy val system: FutureSystem = FutureSystem()
+package test.transport.websocket
 
-//  override def arbitraryContext: Arbitrary[Context] =
-//    HttpContextGenerator.arbitrary
-//
-//  override def serverTransport(
-//    handler: Types.HandlerAnyCodec[Effect, Context],
-//    port: Int
-//  ): ServerMessageTransport[Effect] =
-//    UndertowServer(handler, port)
-//
-//  override def webSocket: Boolean = true
-//
-//  override def run[T](effect: Effect[T]): T = await(effect)
-//}
+import automorph.Types
+import automorph.spi.transport.ServerMessageTransport
+import automorph.system.FutureSystem
+import automorph.transport.http.server.UndertowServer
+import org.scalacheck.Arbitrary
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.Future
+import test.standard.StandardHttpServerTest
+import test.transport.http.HttpContextGenerator
+
+class UndertowWebSocketFutureTest extends StandardHttpServerTest {
+
+  type Effect[T] = Future[T]
+  type Context = UndertowServer.Context
+
+  override lazy val system: FutureSystem = FutureSystem()
+
+  override def arbitraryContext: Arbitrary[Context] =
+    HttpContextGenerator.arbitrary
+
+  override def serverTransport(
+    handler: Types.HandlerAnyCodec[Effect, Context],
+    port: Int
+  ): ServerMessageTransport[Effect] =
+    UndertowServer(handler, port)
+
+  override def webSocket: Boolean = true
+
+  override def run[T](effect: Effect[T]): T = await(effect)
+}
