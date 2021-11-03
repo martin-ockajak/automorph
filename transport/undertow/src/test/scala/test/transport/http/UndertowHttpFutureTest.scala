171873073cac6c91f74e46d3661487c07c70e4cc
Martin Ockajak <m@polytree.org>
2021-11-03 14:29:15 +0100

    Refactoring

--- transport/undertow/src/test/scala/test/transport/http/UndertowHttpFutureTest.scala
+++ transport/undertow/src/test/scala/test/transport/http/UndertowHttpFutureTest.scala
@@ -1,30 +1,30 @@
-//package test.transport.http
-//
-//import automorph.Types
-//import automorph.spi.transport.ServerMessageTransport
-//import automorph.system.FutureSystem
-//import automorph.transport.http.server.UndertowServer
-//import org.scalacheck.Arbitrary
-//import scala.collection.mutable.ArrayBuffer
-//import scala.concurrent.Future
-//import test.standard.StandardHttpServerTest
-//import test.transport.http.HttpContextGenerator
-//
-//class UndertowHttpFutureTest extends StandardHttpServerTest {
-//
-//  type Effect[T] = Future[T]
-//  type Context = UndertowServer.Context
-//
-//  override lazy val system: FutureSystem = FutureSystem()
-//
-//  override def arbitraryContext: Arbitrary[Context] =
-//    HttpContextGenerator.arbitrary
-//
-//  override def serverTransport(
-//    handler: Types.HandlerAnyCodec[Effect, Context],
-//    port: Int
-//  ): ServerMessageTransport[Effect] =
-//    UndertowServer(handler, port)
-//
-//  override def run[T](effect: Effect[T]): T = await(effect)
-//}
+package test.transport.http
+
+import automorph.Types
+import automorph.spi.transport.ServerMessageTransport
+import automorph.system.FutureSystem
+import automorph.transport.http.server.UndertowServer
+import org.scalacheck.Arbitrary
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.Future
+import test.standard.StandardHttpServerTest
+import test.transport.http.HttpContextGenerator
+
+class UndertowHttpFutureTest extends StandardHttpServerTest {
+
+  type Effect[T] = Future[T]
+  type Context = UndertowServer.Context
+
+  override lazy val system: FutureSystem = FutureSystem()
+
+  override def arbitraryContext: Arbitrary[Context] =
+    HttpContextGenerator.arbitrary
+
+  override def serverTransport(
+    handler: Types.HandlerAnyCodec[Effect, Context],
+    port: Int
+  ): ServerMessageTransport[Effect] =
+    UndertowServer(handler, port)
+
+  override def run[T](effect: Effect[T]): T = await(effect)
+}
