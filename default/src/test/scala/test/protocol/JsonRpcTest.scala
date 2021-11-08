10a772659860a5b18ec0bae22495471289774ccd
Martin Ockajak <m@polytree.org>
2021-11-08 01:38:45 +0100

    Refactoring

--- default/src/test/scala/test/protocol/JsonRpcTest.scala
+++ default/src/test/scala/test/protocol/JsonRpcTest.scala
@@ -1,35 +1,21 @@
 package automorph.protocol
 
 import automorph.Default
-import automorph.spi.protocol.{RpcFunction, RpcParameter}
 import test.base.BaseTest
 
 class JsonRpcTest extends BaseTest {
 
-  private val functions = Seq(
-    RpcFunction(
-      "test",
-      Seq(
-        RpcParameter("foo", "String"),
-        RpcParameter("bar", "Integer"),
-        RpcParameter("alt", "Option[Map[String, Boolean]")
-      ),
-      "Seq[String]",
-      Some("Testing function")
-    )
-  )
-
   "" - {
-    "Discovery" - {
+    "API description" - {
       "OpenRPC" in {
         val protocol = JsonRpcProtocol[Default.Node, Default.Codec, Default.ServerContext](Default.codec)
-        //      val specification = protocol.openApi(functions, "Test", "0.0", Seq("http://localhost:80/api"))
-        //      specification.should(not(be(empty)))
+        val description = protocol.apiDescriptions.find(_.function.name == JsonRpcProtocol.openRpcFunction)
+        description.should(not(be(empty)))
       }
       "OpenAPI" in {
         val protocol = JsonRpcProtocol[Default.Node, Default.Codec, Default.ServerContext](Default.codec)
-        //      val specification = protocol.openApi(functions, "Test", "0.0", Seq("http://localhost:80/api"))
-        //      specification.should(not(be(empty)))
+        val description = protocol.apiDescriptions.find(_.function.name == JsonRpcProtocol.openApiFunction)
+        description.should(not(be(empty)))
       }
     }
   }
--- default/src/test/scala/test/protocol/RestRpcTest.scala
+++ default/src/test/scala/test/protocol/RestRpcTest.scala
@@ -1,30 +1,16 @@
 package automorph.protocol
 
 import automorph.Default
-import automorph.spi.protocol.{RpcFunction, RpcParameter}
 import test.base.BaseTest
 
 class RestRpcTest extends BaseTest {
 
-  private val functions = Seq(
-    RpcFunction(
-      "test",
-      Seq(
-        RpcParameter("foo", "String"),
-        RpcParameter("bar", "Integer"),
-        RpcParameter("alt", "Option[Map[String, Boolean]")
-      ),
-      "Seq[String]",
-      Some("Testing function")
-    )
-  )
-
   "" - {
-    "Discovery" - {
+    "API description" - {
       "OpenAPI" in {
-        //      val protocol = RestRpcProtocol[Default.Node, Default.Codec, Default.ServerContext](Default.codec, "/api/")
-        //      val specification = protocol.openApi(functions, "Test", "0.0", Seq("http://localhost:80/api"))
-        //      specification.should(not(be(empty)))
+        val protocol = RestRpcProtocol[Default.Node, Default.Codec, Default.ServerContext](Default.codec, "/api/")
+        val description = protocol.apiDescriptions.find(_.function.name == RestRpcProtocol.openApiFunction)
+        description.should(not(be(empty)))
       }
     }
   }
