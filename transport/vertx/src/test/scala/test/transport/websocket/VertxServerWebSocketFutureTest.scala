f79a14a05a1658657e750d4e16948292794d5cce
Martin Ockajak <m@polytree.org>
2021-11-15 23:03:21 +0100

    Vertx

--- transport/vertx/src/test/scala/test/transport/websocket/VertxServerWebSocketFutureTest.scala
+++ transport/vertx/src/test/scala/test/transport/websocket/VertxServerWebSocketFutureTest.scala
@@ -1,33 +1,33 @@
-//package test.transport.websocket
-//
-//import automorph.Types
-//import automorph.spi.transport.ServerMessageTransport
-//import automorph.system.FutureSystem
-//import automorph.transport.http.server.VertxServer
-//import org.scalacheck.Arbitrary
-//import scala.concurrent.ExecutionContext.Implicits.global
-//import scala.concurrent.Future
-//import test.standard.StandardHttpServerTest
-//import test.transport.http.HttpContextGenerator
-//
-//class VertxServerWebSocketFutureTest extends StandardHttpServerTest {
-//
-//  type Effect[T] = Future[T]
-//  type Context = VertxServer.Context
-//
-//  override lazy val system: FutureSystem = FutureSystem()
-//
-//  override def execute[T](effect: Effect[T]): T =
-//    await(effect)
-//
-//  override def arbitraryContext: Arbitrary[Context] =
-//    HttpContextGenerator.arbitrary
-//
-//  override def serverTransport(
-//    handler: Types.HandlerAnyCodec[Effect, Context],
-//    port: Int
-//  ): ServerMessageTransport[Effect] =
-//    VertxServer(handler, port)
-//
-//  override def webSocket: Boolean = true
-//}
+package test.transport.websocket
+
+import automorph.Types
+import automorph.spi.transport.ServerMessageTransport
+import automorph.system.FutureSystem
+import automorph.transport.http.server.VertxServer
+import org.scalacheck.Arbitrary
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import test.standard.StandardHttpServerTest
+import test.transport.http.HttpContextGenerator
+
+class VertxServerWebSocketFutureTest extends StandardHttpServerTest {
+
+  type Effect[T] = Future[T]
+  type Context = VertxServer.Context
+
+  override lazy val system: FutureSystem = FutureSystem()
+
+  override def execute[T](effect: Effect[T]): T =
+    await(effect)
+
+  override def arbitraryContext: Arbitrary[Context] =
+    HttpContextGenerator.arbitrary
+
+  override def serverTransport(
+    handler: Types.HandlerAnyCodec[Effect, Context],
+    port: Int
+  ): ServerMessageTransport[Effect] =
+    VertxServer(handler, port)
+
+  override def webSocket: Boolean = true
+}
