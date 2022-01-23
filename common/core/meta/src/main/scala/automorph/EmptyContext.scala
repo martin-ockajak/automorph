9a5b518618f895abc98c80495e6c0826283d3025
Martin Ockajak <m@polytree.org>
2022-01-23 23:47:15 +0100

    Refactoring

--- common/core/meta/src/main/scala/automorph/EmptyContext.scala
+++ common/core/meta/src/main/scala/automorph/EmptyContext.scala
@@ -1,17 +1,17 @@
 package automorph
 
 /**
- * Empty RPC request context.
+ * Empty RPC message context.
  *
- * @tparam T empty request context value type
+ * @tparam T empty RPC message context value type
  */
 final case class EmptyContext[T]()
 
 object EmptyContext {
 
-  /** Empty request context value type. */
+  /** Empty RPC message context value type. */
   type Value = EmptyContext[EmptyContext.type]
 
-  /** Implicit empty request context value. */
+  /** Implicit empty RPC message context value. */
   implicit val value: Value = EmptyContext[EmptyContext.type]()
 }
