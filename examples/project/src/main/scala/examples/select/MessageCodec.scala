5340dcb89bc71b7fadcc928616dce6d8595dc12b
Martin Ockajak <m@polytree.org>
2023-02-26 04:59:27 +0100

    Refactoring

--- examples/project/src/main/scala/examples/select/MessageCodec.scala
+++ examples/project/src/main/scala/examples/select/MessageCodec.scala
@@ -18,7 +18,7 @@ private[examples] object MessageCodec {
     val codec = UpickleMessagePackCodec()
 
     // Provide custom data type serialization and deserialization logic
-    implicit def recordRw: codec.custom.ReadWriter[Record] = codec.custom.macroRW
+    implicit def recordRw: codec.custom.ReadWriter[Record] = codec.custom.macroRW[Record]
 
     // Create server API instance
     class ServerApi {
