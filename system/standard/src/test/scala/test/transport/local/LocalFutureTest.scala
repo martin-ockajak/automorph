058a3217d4354d10cdc7dfa1dd67ba7f3a6267e8
Martin Ockajak <m@polytree.org>
2023-03-26 17:35:53 +0200

    -

--- system/standard/src/test/scala/test/transport/local/LocalFutureTest.scala
+++ system/standard/src/test/scala/test/transport/local/LocalFutureTest.scala
@@ -2,22 +2,15 @@ package test.transport.local
 
 import automorph.spi.EffectSystem
 import automorph.system.FutureSystem
-import org.scalacheck.Arbitrary
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import test.base.Await
-import test.core.ProtocolCodecTest
 
-class LocalFutureTest extends ProtocolCodecTest {
+class LocalFutureTest extends LocalTest {
 
   type Effect[T] = Future[T]
-  type Context = Map[String, Double]
 
   override lazy val system: EffectSystem[Effect] = FutureSystem()
 
   override def run[T](effect: Effect[T]): T =
     await(effect)
-
-  override def arbitraryContext: Arbitrary[Context] =
-    Arbitrary(Arbitrary.arbitrary[Context])
 }
--- system/standard/src/test/scala/test/transport/local/LocalTryTest.scala
+++ system/standard/src/test/scala/test/transport/local/LocalTryTest.scala
@@ -2,20 +2,14 @@ package test.transport.local
 
 import automorph.system.TrySystem
 import automorph.spi.EffectSystem
-import org.scalacheck.Arbitrary
 import scala.util.Try
-import test.core.ProtocolCodecTest
 
-class LocalTryTest extends ProtocolCodecTest {
+class LocalTryTest extends LocalTest {
 
   type Effect[T] = Try[T]
-  type Context = Seq[Int]
 
   override lazy val system: EffectSystem[Effect] = TrySystem()
 
   override def run[T](effect: Effect[T]): T =
     effect.get
-
-  override def arbitraryContext: Arbitrary[Context] =
-    Arbitrary(Arbitrary.arbitrary[Context])
 }
