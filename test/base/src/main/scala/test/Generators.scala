652e1f32147f3fc443b0a94c76476ab57e74bd3d
Luigi Antognini <luigi.antognini@produs.ch>
2023-04-14 19:04:51 +0200

    confined generated strings for test to printable ASCII

--- test/base/src/main/scala/test/Generators.scala
+++ test/base/src/main/scala/test/Generators.scala
@@ -3,30 +3,33 @@ package test
 import org.scalacheck.Arbitrary.{
   arbBool, arbByte, arbDouble, arbFloat, arbInt, arbLong, arbOption, arbShort, arbString, arbitrary,
 }
-import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.Arbitrary
+import org.scalacheck.Gen.{asciiPrintableStr, choose, listOf, mapOf, zip}
 import test.Enum.Enum
 
 case object Generators {
 
-  implicit val arbitraryEnum: Arbitrary[Enum] = Arbitrary(Gen.choose(0, Enum.values.size - 1).map(Enum.fromOrdinal))
+  implicit val arbitraryEnum: Arbitrary[Enum] = Arbitrary(choose(0, Enum.values.size - 1).map(Enum.fromOrdinal))
 
   implicit val arbitraryStructure: Arbitrary[Structure] = Arbitrary(for {
-    value <- arbitrary[String]
+    value <- asciiPrintableStr
   } yield Structure(value))
 
-  implicit val arbitraryRecord: Arbitrary[Record] = Arbitrary(for {
-    string <- arbitrary[String]
-    boolean <- arbitrary[Boolean]
-    byte <- arbitrary[Byte]
-    short <- arbitrary[Short]
-    int <- arbitrary[Option[Int]]
-    long <- arbitrary[Long]
-    float <- arbitrary[Float]
-    double <- arbitrary[Double]
-    enumeration <- arbitrary[Enum]
-    list <- arbitrary[List[String]]
-    map <- arbitrary[Map[String, Int]]
-    structure <- arbitrary[Option[Structure]]
-    none <- arbitrary[Option[String]]
-  } yield Record(string, boolean, byte, short, int, long, float, double, enumeration, list, map, structure, none))
+  implicit val arbitraryRecord: Arbitrary[Record] = {
+    Arbitrary(for {
+      string <- asciiPrintableStr
+      boolean <- arbitrary[Boolean]
+      byte <- arbitrary[Byte]
+      short <- arbitrary[Short]
+      int <- arbitrary[Option[Int]]
+      long <- arbitrary[Long]
+      float <- arbitrary[Float]
+      double <- arbitrary[Double]
+      enumeration <- arbitrary[Enum]
+      list <- listOf(asciiPrintableStr)
+      map <- mapOf(zip(asciiPrintableStr, arbitrary[Int]))
+      structure <- arbitrary[Option[Structure]]
+      none <- arbitrary[Option[String]]
+    } yield Record(string, boolean, byte, short, int, long, float, double, enumeration, list, map, structure, none))
+  }
 }
