444c919d472e57a8bdd04ebb7c2d046a98d00c78
Martin Ockajak <m@polytree.org>
2023-04-17 17:54:59 +0200

    .

--- transport/tapir/src/test/scala/test/transport/http/TapirHttp4sHttpFutureTest.scala
+++ transport/tapir/src/test/scala/test/transport/http/TapirHttp4sHttpFutureTest.scala
@@ -3,16 +3,14 @@
 //import automorph.spi.{EffectSystem, RequestHandler, ServerTransport}
 //import automorph.system.FutureSystem
 //import automorph.transport.http.endpoint.TapirHttpEndpoint
-//import com.twitter.finatra.http.routing.HttpRouter
-//import com.twitter.finatra.http.{Controller, HttpServer}
+//import java.net.InetSocketAddress
 //import org.scalacheck.Arbitrary
 //import scala.concurrent.ExecutionContext.Implicits.global
 //import scala.concurrent.Future
-//import sttp.tapir.server.finatra.{FinatraRoute, FinatraServerInterpreter, TapirController}
 //import test.standard.StandardHttpServerTest
-//import test.transport.http.TapirFinatraHttpFutureTest.TapirServer
+//import test.transport.http.TapirHttp4sHttpFutureTest.TapirServer
 //
-//class TapirVertxHttpFutureTest extends StandardHttpServerTest {
+//class TapirHttp4sHttpFutureTest extends StandardHttpServerTest {
 //
 //  type Effect[T] = Future[T]
 //  type Context = TapirHttpEndpoint.Context
@@ -26,50 +24,36 @@
 //    HttpContextGenerator.arbitrary
 //
 //  def serverTransport(id: Int): ServerTransport[Effect, Context] =
-//    FinatraServer(system, port(id))
+//    TapirServer(system, port(id))
 //
 //  override def portRange: Range =
 //    Range(25000, 30000)
 //}
 //
-//case object TapirFinatraHttpFutureTest {
+//case object TapirHttp4sHttpFutureTest {
 //
 //  type Effect[T] = Future[T]
 //  type Context = TapirHttpEndpoint.Context
 //
-//  final case class FinatraController(route: FinatraRoute) extends Controller with TapirController {
-//    addTapirRoute(route)
-//  }
-//
-//  final case class FinatraHttpServer(port: Int) extends HttpServer {
-//
-//    override protected def defaultHttpPort: String =
-//      s":$port"
-//
-//    override protected def configureHttp(router: HttpRouter): Unit =
-//      router.add[FinatraController]
-//  }
-//
 //  final case class TapirServer(effectSystem: EffectSystem[Effect], port: Int) extends ServerTransport[Effect, Context] {
 //    private var endpoint = TapirHttpEndpoint(effectSystem)
-//    private var server = Option.empty[HttpServer]
+//    private var server = Option.empty[Http4sFutureServerBinding[InetSocketAddress]]
 //
 //    override def withHandler(handler: RequestHandler[Effect, Context]): ServerTransport[Effect, Context] = {
 //      endpoint = endpoint.withHandler(handler)
-//      val route = FinatraServerInterpreter().toRoute(endpoint.adapter)
-//      server = FinatraHttpServer()
 //      this
 //    }
 //
-//    override def init(): Effect[Unit] =
-//      effectSystem.evaluate {
-//        server = server.listen(port).toCompletionStage.toCompletableFuture.get()
+//    override def init(): Effect[Unit] = {
+//      Http4sFutureServer().port(port).addEndpoint(endpoint.adapter).start().map { activeServer =>
+//        server = Some(activeServer)
 //      }
+//    }
 //
-//    override def close(): Effect[Unit] =
-//      effectSystem.evaluate {
-//
-//        server.close().toCompletionStage.toCompletableFuture.get()
-//      }
+//    override def close(): Effect[Unit] = {
+//      server.map { activeServer =>
+//        activeServer.stop()
+//      }.getOrElse(effectSystem.successful {})
+//    }
 //  }
 //}
