6a894b10f0cf416ac0d4cc1cde2ff06d822d38a6
Luigi Antognini <luigi.antognini@produs.ch>
2023-04-20 09:34:10 +0200

    lock ports

--- test/system/src/main/scala/test/core/ClientServerTest.scala
+++ test/system/src/main/scala/test/core/ClientServerTest.scala
@@ -3,9 +3,6 @@ package test.core
 import java.net.URI
 import scala.collection.mutable
 import test.base.{Await, Network}
-import test.core.ClientServerTest.usedPorts
-
-import scala.collection.immutable.{SortedMap, SortedSet}
 
 trait ClientServerTest extends ProtocolCodecTest with Await with Network {
   private lazy val ports: mutable.Map[Int, Int] = mutable.HashMap()
@@ -23,12 +20,12 @@ trait ClientServerTest extends ProtocolCodecTest with Await with Network {
 
   private def acquirePort: Int =
     ClientServerTest.usedPorts.synchronized {
-      val port = availablePort(usedPorts.toSet)
+      val port = availablePort(ClientServerTest.usedPorts.toSet)
       ClientServerTest.usedPorts.add(port)
       port
     }
 }
 
 case object ClientServerTest {
-  private val usedPorts = mutable.Set.empty[Int]
+  private val usedPorts = mutable.HashSet[Int]()
 }
