7c4e1c5aa29c980d41ff8bb6f9cd70f75991da84
Luigi Antognini <luigi.antognini@produs.ch>
2023-06-08 14:48:36 +0200

    .

--- test/system/src/main/scala/test/core/ClientServerTest.scala
+++ test/system/src/main/scala/test/core/ClientServerTest.scala
@@ -7,12 +7,20 @@ import test.base.{Await, Network}
 trait ClientServerTest extends ProtocolCodecTest with Await with Network {
   private lazy val ports: mutable.Map[Int, Int] = mutable.HashMap()
 
-  def port(fixtureId: Int): Int =
+  def port(fixtureId: Int): Int = {
+    val objectId = System.identityHashCode(this)
     ports.synchronized {
-      ports.getOrElseUpdate(fixtureId, claimPort())
+      val inCache = ports.isDefinedAt(fixtureId)
+      val port = ports.getOrElseUpdate(fixtureId, claimPort())
+      val source = if (inCache) s"from cache($port)" else s"newly locked port ($port)"
+      println(s"$ClientServerTest@$objectId: asking for port(fixtureId: $fixtureId) -> $source")
+      port
     }
+  }
 
   def url(fixtureId: Int): URI = {
+    val objectId = System.identityHashCode(this)
+    println(s"$ClientServerTest@$objectId: asking for url  (fixtureId: $fixtureId)")
     val scheme = Option.when(webSocket)("ws").getOrElse("http")
     new URI(s"$scheme://localhost:${port(fixtureId)}")
   }
@@ -20,3 +28,5 @@ trait ClientServerTest extends ProtocolCodecTest with Await with Network {
   def webSocket: Boolean =
     false
 }
+
+case object ClientServerTest
