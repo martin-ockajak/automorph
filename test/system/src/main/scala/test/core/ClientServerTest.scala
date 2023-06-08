5280b8ce121fcac10ddefad93c1bc969ed091df7
Luigi Antognini <luigi.antognini@produs.ch>
2023-06-08 14:54:30 +0200

    .

--- test/system/src/main/scala/test/core/ClientServerTest.scala
+++ test/system/src/main/scala/test/core/ClientServerTest.scala
@@ -9,18 +9,20 @@ trait ClientServerTest extends ProtocolCodecTest with Await with Network {
 
   def port(fixtureId: Int): Int = {
     val objectId = System.identityHashCode(this)
+    val className = getClass.getSimpleName
     ports.synchronized {
       val inCache = ports.isDefinedAt(fixtureId)
       val port = ports.getOrElseUpdate(fixtureId, claimPort())
       val source = if (inCache) s"from cache($port)" else s"newly locked port ($port)"
-      println(s"$ClientServerTest@$objectId: asking for port(fixtureId: $fixtureId) -> $source")
+      println(s"$className@$objectId: asking for port(fixtureId: $fixtureId) -> $source")
       port
     }
   }
 
   def url(fixtureId: Int): URI = {
     val objectId = System.identityHashCode(this)
-    println(s"$ClientServerTest@$objectId: asking for url  (fixtureId: $fixtureId)")
+    val className = getClass.getSimpleName
+    println(s"$className@$objectId: asking for url  (fixtureId: $fixtureId)")
     val scheme = Option.when(webSocket)("ws").getOrElse("http")
     new URI(s"$scheme://localhost:${port(fixtureId)}")
   }
@@ -28,5 +30,3 @@ trait ClientServerTest extends ProtocolCodecTest with Await with Network {
   def webSocket: Boolean =
     false
 }
-
-case object ClientServerTest
